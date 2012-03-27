d969b6e764a6db7c27b358cb66524d8f2dbb30f0
Tobias Roeser <le.petit.fou@web.de>
2012-03-27 09:30:01 +0000

    Use properties in test project.

--- de.tototec.sbuild.test1/SBuild.scala
+++ de.tototec.sbuild.test1/SBuild.scala
@@ -4,35 +4,51 @@ import org.apache.tools.ant
 
 class SBuild(implicit P: Project) {
 
+  Prop("java.source", "1.6")
+  Prop("java.target", "1.6")
+  Prop("java.debug", "true")
+  Prop("java.source.dir", "src/main/java")
+  Prop("java.output.dir", "target/classes")
+  Prop("ant.loglevel", ant.Project.MSG_INFO.toString)
+
+  val source = Prop("java.source")
+
   Target("phony:clean") exec {
-    new Delete() {
+    new Delete() { 
+      setProject(P)
       setDir(Path("target"))
     }.execute
-  }
+  } help "Clean all output (target dir)"
 
-  Target("phony:all") dependsOn "target/test.jar"
+  Target("phony:all") dependsOn "target/test.jar" help "Build the project"
 
-  SchemeHandler("mvn", new MvnSchemeHandler(".m2/repository"))
+  // SchemeHandler("mvn", new MvnSchemeHandler(".m2/repository"))
+
+  // Idea: MavenLikeJavaProject()
 
   Target("phony:compile") exec {
     new Mkdir() {
-      setDir(Path("target/classes"))
+      setProject(P)
+      setDir(Path(Prop("java.output.dir")))
     }.execute
 
-    new Javac() {
-      setProject(P.ant)
+    new Javac() { 
+      setProject(P)
       setFork(true)
-      setSrcdir(new ant.types.Path(P.ant) {setLocation(Path("src/main/java"))})
-      setDestdir(P.uniqueFile("target/classes"))
+      setSource(Prop("java.source"))
+      setTarget(Prop("java.target"))
+      setDebug(Prop("java.debug").toBoolean)
+      setIncludeantruntime(false)
+      setSrcdir(new ant.types.Path(P) {setLocation(Path(Prop("java.source.dir")))})
+      setDestdir(Path(Prop("java.output.dir")))
     }.execute
   } 
 
   Target("target/test.jar") dependsOn "compile" exec {
     new Jar() {
-      setProject(P.ant)
-      // setFilesonly(true)
+      setProject(P)
       setDestFile(Path("target/test.jar"))
-      setBasedir(Path("target/classes"))
+      setBasedir(Path(Prop("java.output.dir")))
     }.execute
   }
 
--- de.tototec.sbuild.test1/src/main/java/test/Test.java
+++ de.tototec.sbuild.test1/src/main/java/test/Test.java
@@ -1,4 +1,7 @@
 package test;
 
 public class Test {
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
 }
