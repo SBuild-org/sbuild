ab494bf0e4fdf9df27c9d1afca308441a00dd00f
Tobias Roeser <le.petit.fou@web.de>
2012-03-30 19:22:06 +0000

    Added separate object SetProp(key,value) for setting properties. Prop(key, value) has not getOrElseDefault semantics.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/Prop.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/Prop.scala
@@ -1,8 +1,13 @@
 package de.tototec.sbuild
 
-object Prop {
+object SetProp {
   def apply(key: String, value: String)(implicit project: Project) =
     project.addProperty(key, value)
+}
+
+object Prop {
+  def apply(key: String, value: String)(implicit project: Project) =
+    project.properties.getOrElse(key, value)
   def apply(key: String)(implicit project: Project): String = project.properties(key)
 }
 
