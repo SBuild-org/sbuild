f90d229e00cbf44b9995b70c9532d0ef11222228
Tobias Roeser <le.petit.fou@web.de>
2012-04-01 13:08:01 +0000

    Forgot to add the essential class in last commits.

---
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/TargetRefs.scala
@@ -0,0 +1,18 @@
+package de.tototec.sbuild
+
+import java.io.File
+
+object TargetRefs {
+  implicit def fromString(string: String): TargetRefs = TargetRefs(TargetRef(string))
+  implicit def fromFile(file: File): TargetRefs = TargetRefs(TargetRef("file:" + file.getPath))
+  implicit def fromTargetRef(targetRef: TargetRef): TargetRefs = TargetRefs(targetRef)
+  implicit def fromTarget(target: Target): TargetRefs = TargetRefs(TargetRef(target.name))
+  implicit def fromSeq(targetRefs: Seq[TargetRef]): TargetRefs = TargetRefs(targetRefs: _*)
+}
+
+case class TargetRefs(val targetRefs: TargetRef*) {
+  def /(targetRefs: TargetRefs): TargetRefs = TargetRefs((this.targetRefs ++ targetRefs.targetRefs): _*)
+  def /(targetRef: TargetRef): TargetRefs = TargetRefs((targetRefs ++ Seq(targetRef)): _*)
+  def /(file: File): TargetRefs = TargetRefs((targetRefs ++ Seq(TargetRef(file))): _*)
+  def /(string: String): TargetRefs = TargetRefs((targetRefs ++ Seq(TargetRef(string))): _*)
+}
