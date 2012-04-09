88fcab1bc2bf6a8fe85e7e2796ba16fdf9e9186d
Tobias Roeser <le.petit.fou@web.de>
2012-04-09 08:47:01 +0000

    Added excludes zu AntFileSet.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/ant/AntFileSet.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/ant/AntFileSet.scala
@@ -3,20 +3,34 @@ package de.tototec.sbuild.ant
 import de.tototec.sbuild.Project
 import java.io.File
 import de.tototec.sbuild.Path
+import org.apache.tools.ant.types.FileSet
 
 object AntFileSet {
-  type FileSet = org.apache.tools.ant.types.FileSet
+  type AntFileSet = org.apache.tools.ant.types.FileSet
 
   def apply()(implicit P: Project) = new FileSet() {
     setProject(AntProject())
   }
-  def apply(dir: String, include: Seq[String] = Seq())(implicit P: Project) = new FileSet() {
-    setProject(AntProject())
-    setDir(Path(dir))
-    include match {
-      case Seq() =>
-      case s => setIncludes(s.mkString(" "))
+
+  def apply(dir: String,
+            file: File = null,
+            include: Seq[String] = Seq(),
+            exclude: Seq[String] = Seq())(implicit P: Project) =
+    new FileSet() {
+      setProject(AntProject())
+      setDir(Path(dir))
+      file match {
+        case null =>
+        case f => setFile(f)
+      }
+      include match {
+        case Seq() =>
+        case s => setIncludes(s.mkString(","))
+      }
+      exclude match {
+        case Seq() =>
+        case s => setExcludes(s.mkString(","))
+      }
     }
-  }
 
 }
