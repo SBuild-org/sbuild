3099ae7ec0f6e1b32cbc848da4fac4d53a150009
Tobias Roeser <le.petit.fou@web.de>
2012-04-12 08:26:44 +0000

    Added new project with ant-specific wrappers.

---
+++ de.tototec.sbuild.ant/sbuild.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SBUILDDIR="/home/lefou/work/tototec/sbuild/de.tototec.sbuild"
+TMPDIR="$(mktemp --directory)"
+
+SCALACP="/home/lefou/.m2/repository-tototec/org/scala-lang/scala-library/2.9.1/scala-library-2.9.1.jar:/home/lefou/.m2/repository-tototec/org/scala-lang/scala-compiler/2.9.1/scala-compiler-2.9.1.jar"
+CMDOPTIONCP="/home/lefou/.m2/repository-tototec/de/tototec/de.tototec.cmdoption/0.1.0/de.tototec.cmdoption-0.1.0.jar"
+
+COMPILECP="${TMPDIR}:${SCALACP}:${CMDOPTIONCP}"
+RUNCP="${TMPDIR}:${SCALACP}:${CMDOPTIONCP}"
+
+#DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
+
+cp -r -- ${SBUILDDIR}/target/classes/* ${TMPDIR}
+
+java ${DEBUG} -cp ${RUNCP} de.tototec.sbuild.runner.SBuildRunner --compile-cp "${COMPILECP}" --use-classloader-hack true "$@"
+
+rm -rf ${TMPDIR}
