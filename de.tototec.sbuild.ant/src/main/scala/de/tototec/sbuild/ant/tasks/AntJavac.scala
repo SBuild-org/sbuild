551280c5490cdc5a2e4871b30ca2f6c1b767228b
Tobias Roeser <le.petit.fou@web.de>
2012-04-15 18:32:59 +0000

    Added encoding and optimize to AntJavac and some camelCase alias-method.

--- de.tototec.sbuild.ant/src/main/scala/de/tototec/sbuild/ant/tasks/AntJavac.scala
+++ de.tototec.sbuild.ant/src/main/scala/de/tototec/sbuild/ant/tasks/AntJavac.scala
@@ -12,16 +12,20 @@ object AntJavac {
             fork: java.lang.Boolean = null,
             source: String = null,
             target: String = null,
+            encoding: String = null,
             includeAntRuntime: java.lang.Boolean = null,
-            debug: java.lang.Boolean = null)(implicit project: Project) =
+            debug: java.lang.Boolean = null,
+            optimize: java.lang.Boolean = null)(implicit project: Project) =
     new AntJavac(
       srcDir = srcDir,
       destDir = destDir,
       fork = fork,
       source = source,
       target = target,
+      encoding = encoding,
       includeAntRuntime = includeAntRuntime,
-      debug = debug
+      debug = debug,
+      optimize = optimize
     ).execute
 }
 
@@ -33,18 +37,24 @@ class AntJavac()(implicit _project: Project) extends Javac {
            fork: java.lang.Boolean = null,
            source: String = null,
            target: String = null,
+           encoding: String = null,
            includeAntRuntime: java.lang.Boolean = null,
-           debug: java.lang.Boolean = null)(implicit project: Project) {
+           debug: java.lang.Boolean = null,
+           optimize: java.lang.Boolean = null)(implicit project: Project) {
     this
     if (srcDir != null) setSrcdir(srcDir)
     if (destDir != null) setDestdir(destDir)
     if (fork != null) setFork(fork.booleanValue)
     if (source != null) setSource(source)
     if (target != null) setTarget(target)
+    if (encoding != null) setEncoding(encoding)
     if (includeAntRuntime != null) setIncludeantruntime(includeAntRuntime.booleanValue)
     if (debug != null) setDebug(debug.booleanValue)
+    if (optimize != null) setOptimize(optimize)
   }
-  
+
+  def setDestDir(destDir: File) = setDestdir(destDir)
   def setIncludeAntRuntime(includeAntRuntime: Boolean) = setIncludeantruntime(includeAntRuntime)
+  def setSrcDir(srcDir: APath) = setSrcdir(srcDir)
 
 }
