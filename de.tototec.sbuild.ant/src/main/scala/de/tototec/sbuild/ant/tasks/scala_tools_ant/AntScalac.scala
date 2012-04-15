950239d83a93144d34fa78f52307f87deb271356
Tobias Roeser <le.petit.fou@web.de>
2012-04-15 14:25:56 +0000

    Added wrappers for Scalac and Scaladoc.

---
+++ de.tototec.sbuild.ant/src/main/scala/de/tototec/sbuild/ant/tasks/scala_tools_ant/AntScalac.scala
@@ -0,0 +1,60 @@
+package de.tototec.sbuild.ant.tasks.scala_tools_ant
+
+import de.tototec.sbuild.ant.AntProject
+import de.tototec.sbuild.Project
+import java.io.File
+import org.apache.tools.ant.types.{ Path => APath }
+import scala.tools.ant.Scalac
+
+object AntScalac {
+  def apply(srcDir: APath = null,
+            destDir: File = null,
+            fork: java.lang.Boolean = null,
+            target: String = null,
+            encoding: String = null,
+            deprecation: String = null,
+            unchecked: String = null,
+            classpath: APath = null,
+            force: java.lang.Boolean = null,
+            logging: String = null)(implicit _project: Project) =
+    new AntScalac(
+      srcDir = srcDir,
+      destDir = destDir,
+      fork = fork,
+      target = target,
+      encoding = encoding,
+      deprecation = deprecation,
+      unchecked = unchecked,
+      classpath = classpath,
+      force = force,
+      logging = logging
+    ).execute
+}
+
+class AntScalac()(implicit _project: Project) extends Scalac {
+  setProject(AntProject())
+
+  def this(srcDir: APath = null,
+           destDir: File = null,
+           fork: java.lang.Boolean = null,
+           target: String = null,
+           encoding: String = null,
+           deprecation: String = null,
+           unchecked: String = null,
+           classpath: APath = null,
+           force: java.lang.Boolean = null,
+           logging: String = null)(implicit _project: Project) {
+    this
+    if (srcDir != null) setSrcdir(srcDir)
+    if (destDir != null) setDestdir(destDir)
+    if (fork != null) setFork(fork.booleanValue)
+    if (target != null) setTarget(target)
+    if (encoding != null) setEncoding(encoding)
+    if (deprecation != null) setDeprecation(deprecation)
+    if (unchecked != null) setUnchecked(unchecked)
+    if (classpath != null) setClasspath(classpath)
+    if (force != null) setForce(force.booleanValue)
+    if (logging != null) setLogging(logging)
+  }
+
+}
---
+++ de.tototec.sbuild.ant/src/main/scala/de/tototec/sbuild/ant/tasks/scala_tools_ant/AntScaladoc.scala
@@ -0,0 +1,48 @@
+package de.tototec.sbuild.ant.tasks.scala_tools_ant
+
+import de.tototec.sbuild.Project
+import scala.tools.ant.Scaladoc
+import de.tototec.sbuild.ant.AntProject
+import org.apache.tools.ant.types.{ Path => APath }
+import java.io.File
+
+object AntScaladoc {
+  def apply(srcDir: APath = null,
+            destDir: File = null,
+            fork: java.lang.Boolean = null,
+            target: String = null,
+            encoding: String = null,
+            deprecation: String = null,
+            unchecked: String = null,
+            classpath: APath = null,
+            force: java.lang.Boolean = null,
+            logging: String = null)(implicit _project: Project) =
+    new AntScaladoc(
+      srcDir = srcDir,
+      destDir = destDir,
+      encoding = encoding,
+      deprecation = deprecation,
+      unchecked = unchecked,
+      classpath = classpath
+    ).execute
+}
+
+class AntScaladoc()(implicit _project: Project) extends Scaladoc {
+  setProject(AntProject())
+
+  def this(srcDir: APath = null,
+           destDir: File = null,
+           encoding: String = null,
+           deprecation: String = null,
+           unchecked: String = null,
+           classpath: APath = null)(implicit _project: Project) {
+    this
+    if (srcDir != null) setSrcdir(srcDir)
+    if (destDir != null) setDestdir(destDir)
+    if (encoding != null) setEncoding(encoding)
+    if (deprecation != null) setDeprecation(deprecation)
+    if (unchecked != null) setUnchecked(unchecked)
+    if (classpath != null) setClasspath(classpath)
+  }
+
+}
