951a6a19484cb419026227ef6fdf68b75d5c213d
Tobias Roeser <le.petit.fou@web.de>
2012-04-19 08:59:49 +0000

    Added new task AntExec.

---
+++ de.tototec.sbuild.ant/src/main/scala/de/tototec/sbuild/ant/tasks/AntExec.scala
@@ -0,0 +1,88 @@
+package de.tototec.sbuild.ant.tasks
+
+import org.apache.tools.ant.taskdefs.Execute
+import de.tototec.sbuild.Project
+import de.tototec.sbuild.ant.AntProject
+import java.io.File
+import org.apache.tools.ant.taskdefs.ExecTask
+
+object AntExec {
+  def apply(executable: String = null,
+            args: Array[String] = null,
+            dir: File = null,
+            spawn: java.lang.Boolean = null,
+            os: String = null,
+            osFamily: String = null,
+            timeout: java.lang.Long = null,
+            output: File = null,
+            input: File = null,
+            inputString: String = null,
+            error: File = null,
+            logError: java.lang.Boolean = null,
+            failOnError: java.lang.Boolean = null,
+            resolveExecutable: java.lang.Boolean = null,
+            searchPath: java.lang.Boolean = null,
+            vmLauncher: java.lang.Boolean = null)(implicit _project: Project) =
+    new AntExec(
+      executable = executable,
+      args = args,
+      dir = dir,
+      spawn = spawn,
+      os = os,
+      osFamily = osFamily,
+      timeout = timeout,
+      output = output,
+      input = input,
+      inputString = inputString,
+      error = error,
+      logError = logError,
+      failOnError = failOnError,
+      resolveExecutable = resolveExecutable,
+      searchPath = searchPath,
+      vmLauncher = vmLauncher
+    ).execute
+}
+
+class AntExec()(implicit _project: Project) extends ExecTask {
+  setProject(AntProject())
+
+  def this(executable: String = null,
+           args: Array[String] = null,
+           dir: File = null,
+           spawn: java.lang.Boolean = null,
+           os: String = null,
+           osFamily: String = null,
+           timeout: java.lang.Long = null,
+           output: File = null,
+           input: File = null,
+           inputString: String = null,
+           error: File = null,
+           logError: java.lang.Boolean = null,
+           failOnError: java.lang.Boolean = null,
+           resolveExecutable: java.lang.Boolean = null,
+           searchPath: java.lang.Boolean = null,
+           vmLauncher: java.lang.Boolean = null)(implicit _project: Project) {
+    this
+    if (executable != null) setExecutable(executable)
+    if (args != null) args foreach { arg =>
+      createArg.setValue(arg)
+    }
+    if (dir != null) setDir(dir)
+    if (spawn != null) setSpawn(spawn.booleanValue)
+    if (os != null) setOs(os)
+    if (osFamily != null) setOsFamily(osFamily)
+    if (timeout != null) setTimeout(timeout.longValue)
+    if (output != null) setOutput(output)
+    if (input != null) setInput(input)
+    if (inputString != null) setInputString(inputString)
+    if (error != null) setError(error)
+    if (logError != null) setLogError(logError.booleanValue)
+    if (failOnError != null) setFailonerror(failOnError.booleanValue)
+    if (resolveExecutable != null) setResolveExecutable(resolveExecutable.booleanValue)
+    if (searchPath != null) setSearchPath(searchPath.booleanValue)
+    if (vmLauncher != null) setVMLauncher(vmLauncher.booleanValue)
+  }
+
+  def setFailOnError(failOnError: Boolean) = setFailonerror(failOnError)
+  def setVmLauncher(vmLauncher: Boolean) = setVMLauncher(vmLauncher)
+}
