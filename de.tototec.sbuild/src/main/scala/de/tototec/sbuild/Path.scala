dffcfb01f093567d0a7a86a527e3b745dc9dd469
Tobias Roeser <le.petit.fou@web.de>
2012-09-03 07:20:33 +0000

    Support for more than one path segment. All segments will be concatenated.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/Path.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/Path.scala
@@ -3,13 +3,21 @@ package de.tototec.sbuild
 import java.io.File
 
 object Path {
-  def apply(path: String)(implicit project: Project): File = {
-    val origFile = new File(path)
-    if (origFile.isAbsolute) {
-      origFile.getCanonicalFile
-    } else {
-      val absFile = new File(project.projectDirectory, path)
-      absFile.getCanonicalFile
+  def apply(path: String, pathes: String*)(implicit project: Project): File = {
+    val file = {
+      val origFile = new File(path)
+      if (origFile.isAbsolute) {
+        origFile.getCanonicalFile
+      } else {
+        val absFile = new File(project.projectDirectory, path)
+        absFile.getCanonicalFile
+      }
+    }
+    if(pathes.isEmpty) {
+      file
+    }
+    else {
+      pathes.foldLeft(file)((f, e) => new File(f, e))
     }
   }
 }
