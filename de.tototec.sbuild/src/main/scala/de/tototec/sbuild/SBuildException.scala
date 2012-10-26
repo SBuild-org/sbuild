6615d952d4c66e913915bbbd20b96cca3e3975e3
Tobias Roeser <le.petit.fou@web.de>
2012-10-26 09:49:46 +0000

    Added new exception.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/SBuildException.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/SBuildException.scala
@@ -3,7 +3,7 @@ package de.tototec.sbuild
 import java.io.File
 
 /**
- * Common superclass for specific SBuild exceptions. 
+ * Common superclass for specific SBuild exceptions.
  */
 class SBuildException(msg: String, cause: Throwable = null) extends RuntimeException(msg, cause)
 
@@ -27,13 +27,17 @@ class ExecutionFailedException(msg: String, cause: Throwable = null) extends SBu
 class ProjectConfigurationException(msg: String, cause: Throwable = null) extends SBuildException(msg, cause) with BuildScriptAware
 
 /**
- * An unsupported scheme was used in a target or dependency. 
+ * An unsupported scheme was used in a target or dependency.
  * Usual reasons are typos or forgotten scheme handler registrations.
  */
 class UnsupportedSchemeException(msg: String, cause: Throwable = null) extends SBuildException(msg, cause) with BuildScriptAware
 
-
 /**
- * An unknown target was requested (on command line or as a dependency). 
+ * An unknown target was requested (on command line or as a dependency).
  */
 class TargetNotFoundException(msg: String, cause: Throwable = null) extends SBuildException(msg, cause) with BuildScriptAware
+
+/**
+ * An required but not-configured property was accessed. A re-configuration is neede.
+ */
+class MissingConfigurationException(msg: String, cause: Throwable = null) extends SBuildException(msg, cause) with BuildScriptAware
