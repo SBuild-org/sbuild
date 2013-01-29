05c81dc291f8aea3d1f4259eaaf7a8d1bea12f34
Tobias Roeser <le.petit.fou@web.de>
2013-01-29 13:53:51 +0000

    Module() and Modules() now return a Module resp. Seq[Module]. Added new class Module which can be used to easily create target references to targets in that module. Deprecated Module.apply(String*) as the return value differs form Module.apply(String). Use Modules.apply() instead.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/Module.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/Module.scala
@@ -3,10 +3,22 @@ package de.tototec.sbuild
 import java.io.File
 
 object Module {
-  def apply(dirOrFile: String)(implicit _project: Project): Unit = _project.findOrCreateModule(dirOrFile)
-  def apply(dirOrFiles: String*)(implicit _project: Project): Unit =
-    dirOrFiles.foreach { dirOrFile =>
-      _project.findOrCreateModule(dirOrFile)
-    }
+  def apply(dirOrFile: String)(implicit _project: Project): Module = {
+    _project.findOrCreateModule(dirOrFile)
+    new Module(dirOrFile, _project)
+  }
+  @deprecated(since = "0.3.2.9000", message = "Use Modules() instead.")
+  def apply(dirOrFiles: String*)(implicit _project: Project): Seq[Module] =
+    dirOrFiles.map { dirOrFile => Module(dirOrFile) }
 }
 
+object Modules {
+  def apply(dirOrFiles: String*)(implicit _project: Project): Seq[Module] =
+    dirOrFiles.map { dirOrFile => Module(dirOrFile) }
+}
+
+
+class Module private[sbuild] (dirOrFile: String, project: Project) {
+  def targetRef(targetRefName: String): TargetRef = TargetRef(dirOrFile + "::" + targetRefName)(project)
+  def targetRefs(targetRefNames: String*): TargetRefs = targetRefNames.map(t => targetRef(t))
+}
