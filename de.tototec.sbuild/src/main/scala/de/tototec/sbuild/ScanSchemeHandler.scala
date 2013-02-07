218035c52bc8f91ffac7b82660ca20bd2e5363b2
Tobias Roeser <le.petit.fou@web.de>
2013-02-07 19:23:20 +0000

    First draft of a ScanSchemeHandler.

---
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/ScanSchemeHandler.scala
@@ -0,0 +1,39 @@
+package de.tototec.sbuild
+
+import java.io.File
+
+class ScanSchemeHandler(implicit project: Project)
+    extends SchemeHandlerWithDependencies {
+
+  override def localPath(path: String): String = s"phony:scan-${path}"
+
+  override def resolve(path: String, targetContext: TargetContext) =
+    scan(path).foreach {
+      f => targetContext.targetLastModified = f.lastModified
+    }
+
+  override def dependsOn(path: String): TargetRefs =
+    if (path.contains(";asDependencies"))
+      scan(path).map(f => TargetRef(f)).toSeq
+    else TargetRefs()
+
+  def scan(path: String): Array[File] = {
+    // TODO: parse path pattern, for now, only a simple regex
+    // path := dir [ ';' 'asDependencies' ] [ ';' 'regex=' regex ]
+
+    val nPath =
+      if (path.contains(";asDependencies"))
+        path.replaceFirst(";asDependencies", "")
+      else path
+
+    val (dir, regex) = nPath.split(";", 2) match {
+      case Array(dir) =>
+        (Path(dir), ".*".r)
+      case Array(dir, regex) =>
+        (Path(dir), regex.r)
+    }
+
+    Util.recursiveListFiles(dir, regex)
+  }
+
+}
