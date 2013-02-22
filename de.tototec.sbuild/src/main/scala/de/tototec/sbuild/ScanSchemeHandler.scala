1940a03f1ab5825cf8ba1662d8b061010c3b86a9
Tobias Roeser <le.petit.fou@web.de>
2013-02-22 10:42:00 +0000

    Ensure, even empty results of scan return a non-zero last modified.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/ScanSchemeHandler.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/ScanSchemeHandler.scala
@@ -4,17 +4,15 @@ import java.io.File
 
 class ScanSchemeHandler(implicit project: Project)
     extends SchemeHandler //    extends SchemeHandlerWithDependencies
-    with TransparentSchemeHandler
-    {
+    with TransparentSchemeHandler {
 
   override def localPath(path: String): String = s"phony:scan-${path}"
 
-  override def resolve(path: String, targetContext: TargetContext): Unit =
-    scan(path).foreach {
-      f =>
-        targetContext.attachFile(f)
-        targetContext.targetLastModified = f.lastModified
-    }
+  override def resolve(path: String, targetContext: TargetContext): Unit = {
+    // Ensure, we always report a last modified, even, if we don't find any file
+    targetContext.targetLastModified = 1
+    scan(path).foreach { f => targetContext.attachFile(f) }
+  }
 
   //  override def dependsOn(path: String): TargetRefs =
   //    if (path.contains(";asDependencies"))
