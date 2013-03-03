99c41e4f9b30ae13024b4e261d318929245bc9a1
Tobias Roeser <le.petit.fou@web.de>
2013-03-03 15:40:47 +0000

    Fixed scan scheme handler in case regex were given.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/ScanSchemeHandler.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/ScanSchemeHandler.scala
@@ -3,7 +3,7 @@ package de.tototec.sbuild
 import java.io.File
 
 class ScanSchemeHandler(implicit project: Project)
-    extends SchemeResolver //    extends SchemeHandlerWithDependencies
+    extends SchemeResolver 
     with TransparentSchemeResolver {
 
   override def localPath(path: String): String = s"phony:scan-${path}"
@@ -11,15 +11,10 @@ class ScanSchemeHandler(implicit project: Project)
   override def resolve(path: String, targetContext: TargetContext): Unit = {
     // Ensure, we always report a last modified, even, if we don't find any file
     targetContext.targetLastModified = 1
-    scan(path).foreach { f => targetContext.attachFile(f) }
+    scan(path, targetContext).foreach { f => targetContext.attachFile(f) }
   }
 
-  //  override def dependsOn(path: String): TargetRefs =
-  //    if (path.contains(";asDependencies"))
-  //      scan(path).map(f => TargetRef(f)).toSeq
-  //    else TargetRefs()
-
-  def scan(path: String): Array[File] = {
+  def scan(path: String, targetContext: TargetContext): Array[File] = {
     // TODO: parse path pattern, for now, only a simple regex
     // path := dir [ ';' 'asDependencies' ] [ ';' 'regex=' regex ]
 
@@ -31,8 +26,12 @@ class ScanSchemeHandler(implicit project: Project)
     val (dir, regex) = nPath.split(";", 2) match {
       case Array(dir) =>
         (Path(dir), ".*".r)
-      case Array(dir, regex) =>
-        (Path(dir), regex.r)
+      case Array(dir, regex) if regex.startsWith("regex=") =>
+        (Path(dir), regex.substring(6).r)
+      case _ =>
+        val ex = new ProjectConfigurationException("Unsupported syntax used in target/depenency: " + targetContext.name)
+        ex.buildScript = Some(targetContext.project.projectFile)
+        throw ex 
     }
 
     Util.recursiveListFiles(dir, regex)
