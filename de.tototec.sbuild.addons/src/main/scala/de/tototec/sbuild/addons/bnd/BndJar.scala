89f3752f341eb7f4d5a212038d09123de80e1978
Tobias Roeser <le.petit.fou@web.de>
2013-03-05 07:34:12 +0000

    Added support for newer Bndlib API.

--- de.tototec.sbuild.addons/src/main/scala/de/tototec/sbuild/addons/bnd/BndJar.scala
+++ de.tototec.sbuild.addons/src/main/scala/de/tototec/sbuild/addons/bnd/BndJar.scala
@@ -36,28 +36,39 @@ class BndJar(
         cl
     }
 
-    val builderClassName = "aQute.lib.osgi.Builder"
-    val builderClass = bndClassLoader.loadClass(builderClassName)
+    val (builderClass, jarClass) = try {
+      val builderClassName = "aQute.lib.osgi.Builder"
+      val builderClass = bndClassLoader.loadClass(builderClassName)
+      val jarClassName = "aQute.lib.osgi.Jar"
+      val jarClass = bndClassLoader.loadClass(jarClassName)
+      (builderClass, jarClass)
+    } catch {
+      case e: ClassNotFoundException =>
+        val builderClassName = "aQute.bnd.osgi.Builder"
+        project.log.log(LogLevel.Debug, s"""Could not found class "${e.getMessage}". Trying new package name (since 2.0.0) "aQute.bnd.ogsi".""")
+        val builderClass = bndClassLoader.loadClass(builderClassName)
+        val jarClassName = "aQute.bnd.osgi.Jar"
+        val jarClass = bndClassLoader.loadClass(jarClassName)
+        (builderClass, jarClass)
+    }
     val builder = builderClass.newInstance
 
     val setBaseMethod = builderClass.getMethod("setBase", classOf[File])
     setBaseMethod.invoke(builder, project.projectDirectory)
 
     val addClasspathMethod = builderClass.getMethod("addClasspath", classOf[File])
-    if(classpath != null) classpath.foreach { file =>
+    if (classpath != null) classpath.foreach { file =>
       addClasspathMethod.invoke(builder, file)
     }
 
     val setPropertyMethod = builderClass.getMethod("setProperty", classOf[String], classOf[String])
-    if(props != null) props.foreach {
+    if (props != null) props.foreach {
       case (key, value) => setPropertyMethod.invoke(builder, key, value)
     }
 
     val buildMethod = builderClass.getMethod("build")
     val jar = buildMethod.invoke(builder)
 
-    val jarClassName = "aQute.lib.osgi.Jar"
-    val jarClass = bndClassLoader.loadClass(jarClassName)
     val writeMethod = jarClass.getMethod("write", classOf[File])
 
     writeMethod.invoke(jar, destFile)
