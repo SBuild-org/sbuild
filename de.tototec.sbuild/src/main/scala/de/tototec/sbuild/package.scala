219b94e7ff93591eefa8de0b86ef38bc772a939c
Tobias Roeser <le.petit.fou@web.de>
2013-03-09 14:08:52 +0000

    Added package scaladoc for de.tototec.sbuild.

---
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/package.scala
@@ -0,0 +1,52 @@
+package de.tototec
+
+/**
+ * ==SBuild API Reference==
+ * 
+ * This package contains SBuild core classes and public API.
+ * 
+ * Project Homepage: [[http://sbuild.tototec.de/]]
+ *
+ * SBuild is released under the [[http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0]].
+ *
+ * 
+ * ===SBuild API to be used in SBuild build scripts===
+ * 
+ * The following classes form SBUild's public API:
+ * 
+ *  - `[[classpath @classpath]]`
+ *  - `[[ExportDependencies]]`
+ *  - `[[include @include]]`
+ *  - `[[Module]]`
+ *  - `[[Modules]]`
+ *  - `[[Path]]`
+ *  - `[[Prop]]` 
+ *  - `[[SchemeHandler]]`
+ *  - `[[SchemeResolver]]`
+ *  - `[[SchemeResolverWithDependencies]]`
+ *  - `[[SetProp]]`
+ *  - `[[Target]]`
+ *  - `[[TargetContext]]`
+ *  - `[[TargetRef]]`
+ *  - `[[TargetRefs]]`
+ *  - `[[version @version]]`
+ * 
+ * Classes and objects, not listed above are candidates to change in succeeding releases. 
+ * You are encouraged, to use only the API form above in your build scripts.
+ * 
+ * ===Default Scheme Handlers===
+ * 
+ * The following list of `[[SchemeHandler]]` implementations are registered by-default with their default configuration in each SBuild project.
+ * To register or overwrite a scheme handler in your project, you should use `[[SchemeHandler$.apply]]`.   
+ * 
+ *  - `[[HttpSchemeHandler]]`
+ *  - `[[MvnSchemeHandler]]`
+ *  - `[[ZipSchemeHandler]]`
+ *  - `[[ScanSchemeHandler]]`
+ * 
+ * @version 0.4.0
+ * 
+ */
+package object sbuild {
+
+}
