5d3dda45bd49395bd4db85bba74e91d2675d5437
Tobias Roeser <le.petit.fou@web.de>
2013-03-25 17:05:25 +0000

    Added factory for compiler classpath. Reviewed scaladoc.

--- de.tototec.sbuild.addons/src/main/scala/de/tototec/sbuild/addons/scala/Scalac.scala
+++ de.tototec.sbuild.addons/src/main/scala/de/tototec/sbuild/addons/scala/Scalac.scala
@@ -7,9 +7,13 @@ import de.tototec.sbuild.LogLevel
 import de.tototec.sbuild.ExecutionFailedException
 import de.tototec.sbuild.Util
 import de.tototec.sbuild.Path
+import de.tototec.sbuild.TargetRefs
+import de.tototec.sbuild.TargetRefs._
 import java.io.InputStream
 import java.io.OutputStream
 import de.tototec.sbuild.addons.support.ForkSupport
+import de.tototec.sbuild.ProjectConfigurationException
+import de.tototec.sbuild.LogLevel
 
 /**
  * Companion object for [[Scalac]], the Scala Compiler Addon.
@@ -58,6 +62,21 @@ object Scalac {
       sources = sources
     ).execute
 
+  def compilerClasspath(scalaVersion: String)(implicit project: Project): TargetRefs = scalaVersion match {
+    case scala_2_9_like if scala_2_9_like.startsWith("2.9.") || scala_2_9_like.startsWith("2.8.") || scala_2_9_like.startsWith("2.7.") =>
+      s"mvn:org.scala-lang:scala-library:${scalaVersion}" ~
+        s"mvn:org.scala-lang:scala-compiler:${scalaVersion}"
+    case scala_2_10 if scala_2_10.startsWith("2.10.") =>
+      s"mvn:org.scala-lang:scala-library:${scalaVersion}" ~
+        s"mvn:org.scala-lang:scala-compiler:${scalaVersion}" ~
+        s"mvn:org.scala-lang:scala-reflect:${scalaVersion}"
+    case _ =>
+      project.log.log(LogLevel.Warn, "Unsupported Scala version specified: " + scalaVersion + ". Returning only a guessed compiler classpath")
+      s"mvn:org.scala-lang:scala-library:${scalaVersion}" ~
+        s"mvn:org.scala-lang:scala-compiler:${scalaVersion}" ~
+        s"mvn:org.scala-lang:scala-reflect:${scalaVersion}"
+  }
+
 }
 
 /**
@@ -71,7 +90,7 @@ object Scalac {
  * {{{
  * import de.tototec.sbuild._
  * import de.tototec.sbuild.TargetRef._
- * 
+ *
  * @version("0.4.0")
  * class SBuild(implicit _project: Project) {
  *
@@ -104,7 +123,7 @@ object Scalac {
  * }}}
  *
  * @since 0.3.0
- * 
+ *
  * @constructor
  * Create a new Scalac Compiler addon instance. All parameters can be omitted and set later.
  *
@@ -116,38 +135,38 @@ object Scalac {
  *
  *
  * @param compilerClasspath The classpath which contains the compiler and its dependencies. (E.g. scala-compiler.jar, scala-reflect.jar, ...)
- *   If not specified, the compiler must be in the SBuild classpath, e.g. by adding it with `@classpath` annotation. 
+ *   If not specified, the compiler must be in the SBuild classpath, e.g. by adding it with `@classpath` annotation.
  *   If `fork` option is also set to `true`, this parameter is required.
- * @param classpath The classpath is forwarded to the compiler and used to load dependencies of the sources. 
+ * @param classpath The classpath is forwarded to the compiler and used to load dependencies of the sources.
  *   It must also contain the scala library.
- * @param srcDir A directory containing Scala (and Java) source files. 
+ * @param srcDir A directory containing Scala (and Java) source files.
  *   If multiple directories are needed, `srcDirs` parameter can be used.
  * @param srcDirs Multiple directories containing Scala and Java source files.
  *   If only one directory is needed, `srcDir` parameter can be used for convenience.
  * @param sources Source files to be compiled. ''Since 0.4.0''
- * @param destDir The directory, where the compiled class files will be stored. 
+ * @param destDir The directory, where the compiled class files will be stored.
  *   If the directory does not exists, it will be created.
  * @param encoding The encoding of the source files.
  * @param unchecked If `true`, the compiler enables detailed unchecked (erasure) warnings.
  * @param deprecation If `true` , the compiler emit warning and location for usages of deprecated APIs.
  * @param verbose If `true`, the compiler outputs messages about what it is doing.
  * @param target Target platform for object files.
- *   The supported values depend on the used version of the Scalac compiler. 
+ *   The supported values depend on the used version of the Scalac compiler.
  *   Scalac up to 2.9.x supports the following values: jvm-1.5 (default), msil.
  *   Scalac 2.10.x also supports jvm-1.6 (default), jvm-1.7 and various others.
  *   Please consult the Scalac documentation.
- * @param debugInfo The level of generated debugging info. 
+ * @param debugInfo The level of generated debugging info.
  *   Supported values: none, source, line, vars (default), notailcalls.
- *   "none" generates no debugging info, 
- *   "source" generates only the source file attribute, 
- *   "line" generates source and line number information, 
- *   "vars" generates source, line number and local variable information, 
+ *   "none" generates no debugging info,
+ *   "source" generates only the source file attribute,
+ *   "line" generates source and line number information,
+ *   "vars" generates source, line number and local variable information,
  *   "notc" generates all of the above and will not perform tail call optimization.
- * @param fork If `true` runs the compiler in a separate process. 
+ * @param fork If `true` runs the compiler in a separate process.
  *   If not set or set to `false`, the Scala version of SBuild and the used Scala compiler must be binary compatible.
  *   If you indent to use a different (binary incompatible) Scala version to the one SBuild runs with, you should set it to `true`.
  *   If `true`, also the `compilerClasspath` parameter must be specified.
- * @param additionalScalacArgs Additional arguments directly passed to the Scala compiler. 
+ * @param additionalScalacArgs Additional arguments directly passed to the Scala compiler.
  *   Refer to the scalac manual or inspect `scalac -help` output.
  *
  */
