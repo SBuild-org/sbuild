300bc2b7443ab9d0b257bfa6e788457971fbb7b8
Tobias Roeser <le.petit.fou@web.de>
2013-04-07 18:10:49 +0000

    Updated build and improved JavaPlugin.

---
+++ de.tototec.sbuild.plugins/SBuild.scala
@@ -0,0 +1,52 @@
+import de.tototec.sbuild._
+import de.tototec.sbuild.TargetRefs._
+import de.tototec.sbuild.ant._
+import de.tototec.sbuild.ant.tasks._
+import de.tototec.sbuild.addons.scala.Scalac
+
+@version("0.4.0")
+@classpath("mvn:org.apache.ant:ant:1.9.0")
+@include("../SBuildConfig.scala")
+class SBuild(implicit _project: Project) {
+
+  val scalaVersion = SBuildConfig.scalaVersion
+  val sbuildVersion = SBuildConfig.sbuildVersion
+
+  val jar = s"target/de.tototec.sbuild.plugins-${sbuildVersion}.jar"
+
+  val compilerCp =
+    s"mvn:org.scala-lang:scala-library:${scalaVersion}" ~
+    s"mvn:org.scala-lang:scala-compiler:${scalaVersion}" ~
+    s"mvn:org.scala-lang:scala-reflect:${scalaVersion}"
+
+  val compileCp =
+    s"mvn:org.scala-lang:scala-library:${scalaVersion}" ~
+      s"../de.tototec.sbuild/target/de.tototec.sbuild-${sbuildVersion}.jar" ~
+      s"../de.tototec.sbuild.addons/target/de.tototec.sbuild.addons-${sbuildVersion}.jar" ~
+      s"../de.tototec.sbuild.ant/target/de.tototec.sbuild.ant-${sbuildVersion}.jar" ~
+      "mvn:org.apache.ant:ant:1.8.4"
+
+  ExportDependencies("eclipse.classpath", compileCp)
+
+  Target("phony:clean").evictCache exec {
+    AntDelete(dir = Path("target"))
+  }
+
+  Target("phony:all") dependsOn "jar"
+  Target("phony:jar") dependsOn jar
+
+  Target("phony:compile").cacheable dependsOn compileCp ~ compilerCp ~ "scan:src/main/scala" exec {
+    Scalac(
+      compilerClasspath = compilerCp.files,
+      classpath = compileCp.files,
+      sources = "scan:src/main/scala".files,
+      destDir = Path("target/classes"),
+      debugInfo = "vars"
+    )
+  }
+
+  Target(jar) dependsOn "compile" exec { ctx: TargetContext =>
+    AntJar(destFile = ctx.targetFile.get, baseDir = Path("target/classes"))
+  }
+
+}
--- de.tototec.sbuild.plugins/src/main/scala/de/tototec/sbuild/plugins/JavaPlugin.scala
+++ de.tototec.sbuild.plugins/src/main/scala/de/tototec/sbuild/plugins/JavaPlugin.scala
@@ -1,44 +1,117 @@
 package de.tototec.sbuild.plugins
 
 import de.tototec.sbuild.Plugin
+import de.tototec.sbuild.TargetRef
 import de.tototec.sbuild.TargetRefs
 import de.tototec.sbuild.TargetRefs._
 import de.tototec.sbuild.Project
 import de.tototec.sbuild.Target
 import de.tototec.sbuild.Util
+import de.tototec.sbuild.Util.NullSafe
 import de.tototec.sbuild.Path
 import de.tototec.sbuild.addons.java.Javac
 
 class JavaPlugin()(implicit _project: Project) extends Plugin {
 
+  // compile
+
+  var compileCp: TargetRefs = null
+  var testCp: TargetRefs = null
+
+  var sourceDirs: Seq[String] = Seq("src/main/java")
+  var testSourceDirs: Seq[String] = Seq("src/test/java")
+
+  var additionalSources: TargetRefs = TargetRefs()
+  var additionalTestSources: TargetRefs = TargetRefs()
+
+  var compileDependsOn: TargetRefs = TargetRefs()
+  var testDependsOn: TargetRefs = TargetRefs()
+  
   var classesDir = "target/classes"
   var testClassesDir = "target/test-classes"
-  var compileCp: TargetRefs = "compileCp"
-  var testCompileCp: TargetRefs = "testCompileCp"
+
+  var source: String = null
+  var sourcePattern: String = null
+  var target: String = null
+  var encoding: String = "UTF-8"
+  var javacCacheable: Boolean = true
+
+  var javacCustomizer: Javac => Unit = null
+  var testJavacCustomizer: Javac => Unit = null
+
+  // Test Compile
 
   override def init {
+    // ensure these deps are defined and the targets exists
+    compileCp = compileCp orElse Target("phony:compileCp")
+    testCp = testCp orElse Target("phony:testCp")
 
     // TODO: refactor targets out for later use
+    initCompileJava
+    initCompileTestJava
+    initCleanJava
+    initCleanTestJava
+  }
 
+  protected def initCompileJava {
+
+    val sources: TargetRefs = sourceDirs.map { dir => TargetRef("scan:" + dir) }
+
+    val t = Target("phony:compileJava").cacheable dependsOn 
+      compileDependsOn ~ compileCp ~ sources ~ additionalSources exec {
+      val javac = new Javac(
+        classpath = compileCp.files,
+        sources = sources.files ++ additionalSources.files,
+        destDir = Path(classesDir),
+        source = source,
+        target = target,
+        encoding = encoding
+      )
+      javacCustomizer.notNull(c => c(javac))
+      javac.execute
+    }
+
+    Target("phony:compile") dependsOn t
+  }
+
+  protected def initCleanJava {
     val cleanJavaClasses = Target("phony:cleanJavaClasses") evictCache "compileMainJava" exec {
       Util.delete(Path(classesDir))
     }
-
     Target("phony:clean") dependsOn cleanJavaClasses
+  }
 
-    Target("phony:compileJava").cacheable exec {
+  protected def initCompileTestJava {
+    val sources: TargetRefs = testSourceDirs.map { dir => TargetRef("scan:" + dir) }
 
+    val t = Target("phony:compileTestJava").cacheable dependsOn 
+      testDependsOn ~ testCp ~ sources ~ additionalTestSources exec {
+      val sourceFiles = sources.files ++ additionalTestSources.files
+      if (!sourceFiles.isEmpty) {
+        val javac = new Javac(
+          classpath = testCp.files,
+          sources = sourceFiles,
+          destDir = Path(testClassesDir),
+          source = source,
+          target = target,
+          encoding = encoding
+        )
+        testJavacCustomizer.notNull(c => c(javac))
+        javac.execute
+      }
+      else {
+        println("No test sources found")
+      }
     }
 
+    Target("phony:compile") dependsOn t
+  }
+
+  protected def initCleanTestJava {
     val cleanJavaTestClasses = Target("phony:cleanJavaTestClasses") evictCache "compileTestJava" exec {
       Util.delete(Path(testClassesDir))
     }
     Target("phony:clean") dependsOn cleanJavaTestClasses
-
-    Target("phony:compileTestjava").cacheable exec {
-
-    }
-
   }
 
 }
