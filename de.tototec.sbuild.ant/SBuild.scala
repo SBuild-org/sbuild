6c482e3b63260881dc5d58c22b59470debcd84ee
Tobias Roeser <le.petit.fou@web.de>
2013-04-17 12:19:21 +0000

    Build settings.

--- de.tototec.sbuild.ant/SBuild.scala
+++ de.tototec.sbuild.ant/SBuild.scala
@@ -50,7 +50,7 @@ class SBuild(implicit _project: Project) {
     )
   }
 
-  Target("phony:scaladoc") dependsOn SBuildConfig.compilerPath ~ compileCp ~ "scan:src/main/scala" exec {
+  Target("phony:scaladoc").cacheable dependsOn SBuildConfig.compilerPath ~ compileCp ~ "scan:src/main/scala" exec {
     addons.scala.Scaladoc(
       scaladocClasspath = SBuildConfig.compilerPath.files,
       classpath = compileCp.files,
