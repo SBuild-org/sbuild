f7c49ca28849de94d4a5a212a1bc7f48c8b92ec4
Tobias Roeser <le.petit.fou@web.de>
2013-05-01 20:42:17 +0000

    Added new scheme handler, that is able transform the path into another path. Its main purpose is to translate schemes and pathes e.g. to find the source jar that belongs to a binary dependency (e.g. "source:mvn:a:b:1" -> "mvn:a.b:1;classifier=sources").

---
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/MapperSchemeHandler.scala
@@ -0,0 +1,25 @@
+package de.tototec.sbuild
+
+/**
+ * A MapperSchemeHandler will maintain an mapping from on scheme to another scheme.
+ */
+class MapperSchemeHandler(
+  schemeMapping: Seq[(String, String)] = Seq(),
+  pathTranslators: Seq[(String, String => String)] = Seq())(implicit project: Project)
+    extends SchemeHandler {
+
+  override def localPath(path: String): String =
+    schemeMapping.find { case (scheme, _) => path.startsWith(scheme + ":") } match {
+      case Some((scheme, sourceScheme)) =>
+        sourceScheme + path.substring(scheme.size, path.size)
+      case None =>
+        pathTranslators.find { case (scheme, _) => path.startsWith(scheme + ":") } match {
+          case Some((scheme, pathTranslator)) =>
+            scheme + ":" + pathTranslator(path.substring(scheme.size + 1, path.size))
+          case None =>
+            throw new TargetNotFoundException(s"""Cannot find a source scheme handler (mapping or translator) for "${path}".""")
+        }
+
+    }
+
+}
