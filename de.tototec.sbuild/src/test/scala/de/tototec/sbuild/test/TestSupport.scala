a6b874538ed84c45d574c191cebdc882458c44c3
Tobias Roeser <le.petit.fou@web.de>
2013-05-13 08:45:34 +0000

    Added another test support function.

--- de.tototec.sbuild/src/test/scala/de/tototec/sbuild/test/TestSupport.scala
+++ de.tototec.sbuild/src/test/scala/de/tototec/sbuild/test/TestSupport.scala
@@ -3,6 +3,10 @@ package de.tototec.sbuild.test
 import java.io.File
 import de.tototec.sbuild.Project
 import de.tototec.sbuild.BuildFileProject
+import java.io.FileOutputStream
+import java.io.BufferedOutputStream
+import java.io.PrintStream
+import de.tototec.sbuild.BuildFileProject
 
 object TestSupport {
 
@@ -14,6 +18,15 @@ object TestSupport {
 
   def createMainProject: Project = {
     new BuildFileProject(createProjectFile)
+  }
+
+  def createMainProject(content: String): Project = {
+    val file = createProjectFile
+    val stream = new PrintStream(new BufferedOutputStream(new FileOutputStream(file)))
+    stream.print(content)
+    stream.close()
+
+    new BuildFileProject(file)
   }
 
 }
