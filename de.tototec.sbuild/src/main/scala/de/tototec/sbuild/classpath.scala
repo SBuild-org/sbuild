5faeb94ba8e02f8f0be17cb8965e08e8d015fb95
Tobias Roeser <le.petit.fou@web.de>
2013-07-04 22:29:02 +0000

    Scaladoc.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/classpath.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/classpath.scala
@@ -3,8 +3,31 @@ package de.tototec.sbuild
 import scala.annotation.Annotation
 
 /**
- * Use this annotation to add additional classpath items to the execution environment of the current build file.
- * 
- * TODO: also add a @include annotation to specify source files to be included and compiled
+ * Add additional classpath elements (jars, dirs) to the compile and execution environment of the current build file.
+ * This is required to used foreign libraries and APIs in your build script,
+ * otherwise you would be limited to the standard API of the Java, Scala and SBuild distribution.
+ *
+ * The `@classpath` annotation has only effect when used in a build file.
+ *
+ * The `@classpath` annotation supports local files and all default scheme handlers.
+ *
+ * Example of a build file which uses two additional classpath resources, one via the "mvn" scheme handler and one local file:
+ * {{{
+ * import de.tototec.sbuild,_
+ *
+ * @classpath(
+ *   "mvn:org.apache.ant:ant:1.8.4",
+ *   "../common/buildtasks.jar"
+ * )
+ * class SBuild(implicit _project: Project) {
+ *
+ * }
+ *
+ * }}}
+ *
+ * @param value One or more resources to be included into the classpath.
+ *   All default scheme handlers are supported.
+ *
+ * @see [[de.tototec.sbuild.include @include]]
  */
 class classpath(value: String*) extends Annotation 
--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/include.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/include.scala
@@ -2,4 +2,46 @@ package de.tototec.sbuild
 
 import scala.annotation.Annotation
 
+/**
+ * Include one ore more Scala files to access them from the current project.
+ * The included files will be automatically compiled and added to the current classpath of the project.
+ *
+ * The `@include` annotation has only effect when used in a build file. 
+ * Additional compile and runtime dependencies of the included file can be added with the [[de.tototec.sbuild.classpath @classpath annotation]]. 
+ *
+ * The `@include` annotation supports local files and all default scheme handlers.
+ *
+ * Example of a build file with an included file:
+ *
+ * {{{
+ * import de.tototec.sbuild._
+ * 
+ * @include("../SharedConfig.scala")
+ * class SBuild(implicit _project: Project) {
+ *   // ...
+ * }
+ * }}}
+ *
+ * Classes and objects from included files may refer to their current directory, e.g. to access file system resources.
+ * This can be easily accomplished by using [[de.tototec.sbuild.Path$.apply]].
+ *
+ * Example of an included file, utilizing [[de.tototec.sbuild.Path$.apply]]:
+ * {{{
+ * // ... in an included file
+ *
+ * object SharedConfig {
+ *
+ *   val version = "1.0.0"
+ *
+ *   def productLogoFile(implicit _project: Project) = Path[SharedConfig.type]("images/logo.jpg")
+ *
+ * }
+ * }}}
+ *
+ * @param value One or more resources to be included.
+ *   All default scheme handlers are supported.
+ *
+ * @see [[de.tototec.sbuild.classpath @classpath Annotation]]
+ * @see [[de.tototec.sbuild.Path$ Path]]
+ */
 class include(value: String*) extends Annotation 
