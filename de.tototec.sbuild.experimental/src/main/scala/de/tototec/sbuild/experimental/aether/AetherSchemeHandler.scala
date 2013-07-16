822acf3a385fa6840cbcaf7891a9767ec7b40d57
Tobias Roeser <le.petit.fou@web.de>
2013-07-16 20:54:27 +0000

    Improved AetherSchemeHandler. The implementation is now loaded from a separate classpath.

---
+++ de.tototec.sbuild.experimental/src/main/scala/de/tototec/sbuild/experimental/aether/AetherSchemeHandler.scala
@@ -0,0 +1,97 @@
+package de.tototec.sbuild.experimental.aether
+
+import java.io.File
+import java.net.URLClassLoader
+
+import de.tototec.sbuild.LogLevel
+import de.tototec.sbuild.MavenSupport.MavenGav
+import de.tototec.sbuild.Project
+import de.tototec.sbuild.SchemeResolver
+import de.tototec.sbuild.TargetContext
+import de.tototec.sbuild.TargetRefs
+import de.tototec.sbuild.TargetRefs.fromString
+
+object AetherSchemeHandler {
+
+  case class Repository(name: String, layout: String, url: String)
+  val CentralRepo = Repository("central", "default", "http://repo1.maven.org/maven2")
+
+  def fullAetherCp(implicit project: Project): TargetRefs = {
+    val aetherVersion = "0.9.0.M2"
+    val wagonVersion = "2.4"
+
+    import TargetRefs._
+
+    s"mvn:org.eclipse.aether:aether-api:${aetherVersion}" ~
+      s"mvn:org.eclipse.aether:aether-spi:${aetherVersion}" ~
+      s"mvn:org.eclipse.aether:aether-util:${aetherVersion}" ~
+      s"mvn:org.eclipse.aether:aether-impl:${aetherVersion}" ~
+      s"mvn:org.eclipse.aether:aether-connector-file:${aetherVersion}" ~
+      s"mvn:org.eclipse.aether:aether-connector-asynchttpclient:${aetherVersion}" ~
+      s"mvn:org.eclipse.aether:aether-connector-wagon:${aetherVersion}" ~
+      "mvn:io.tesla.maven:maven-aether-provider:3.1.2" ~
+      s"mvn:org.apache.maven.wagon:wagon-provider-api:${wagonVersion}" ~
+      s"mvn:org.apache.maven.wagon:wagon-http:${wagonVersion}" ~
+      s"mvn:org.apache.maven.wagon:wagon-file:${wagonVersion}" ~
+      s"mvn:org.apache.maven.wagon:wagon-ssh:${wagonVersion}" ~
+      "mvn:org.sonatype.maven:wagon-ahc:1.2.1" ~
+      s"mvn:org.apache.maven.wagon:wagon-http-shared4:${wagonVersion}" ~
+      s"mvn:org.codehaus.plexus:plexus-component-annotations:1.5.5" ~
+      s"mvn:org.apache.httpcomponents:httpclient:4.2.5" ~
+      s"mvn:org.apache.httpcomponents:httpcore:4.2.4" ~
+      "mvn:javax.inject:javax.inject:1" ~
+      "mvn:com.ning:async-http-client:1.6.5" ~
+      "mvn:io.tesla.maven:maven-model:3.1.0" ~
+      "mvn:io.tesla.maven:maven-model-builder:3.1.0" ~
+      "mvn:io.tesla.maven:maven-repository-metadata:3.1.0" ~
+      "mvn:org.jboss.netty:netty:3.2.5.Final" ~
+      "mvn:org.eclipse.sisu:org.eclipse.sisu.inject:0.0.0.M1" ~
+      "mvn:org.eclipse.sisu:org.eclipse.sisu.plexus:0.0.0.M1" ~
+      "mvn:org.codehaus.plexus:plexus-classworlds:2.4" ~
+      "mvn:org.codehaus.plexus:plexus-interpolation:1.16" ~
+      "mvn:org.codehaus.plexus:plexus-utils:2.1" ~
+      "mvn:org.sonatype.sisu:sisu-guava:0.9.9" ~
+      "mvn:org.sonatype.sisu:sisu-guice:3.1.0" ~
+      "mvn:org.slf4j:slf4j-api:1.6.2"
+  }
+
+}
+
+class AetherSchemeHandler(
+  aetherClasspath: Seq[File] = Seq(),
+  localRepoDir: File = new File(System.getProperty("user.home") + "/.m2/repository"),
+  remoteRepos: Seq[AetherSchemeHandler.Repository] = Seq(AetherSchemeHandler.CentralRepo))(implicit project: Project)
+    extends SchemeResolver {
+
+  private[this] val worker: AetherSchemeHandlerWorker = {
+
+    val thisClass = classOf[AetherSchemeHandler]
+
+    val aetherClassLoader = aetherClasspath match {
+      case null | Seq() => thisClass.getClassLoader
+      case cp =>
+        val cl = new URLClassLoader(cp.map { f => f.toURI().toURL() }.toArray, thisClass.getClassLoader)
+        project.log.log(LogLevel.Debug, "Using aether classpath: " + cl.getURLs().mkString(", "))
+        cl
+    }
+
+    val workerImplClass = aetherClassLoader.loadClass(thisClass.getPackage().getName() + "." + "impl.AetherSchemeHandlerWorkerImpl")
+    val workerImplClassCtr = workerImplClass.getConstructor(classOf[File], classOf[Seq[AetherSchemeHandler.Repository]])
+    val worker = workerImplClassCtr.newInstance(localRepoDir, remoteRepos).asInstanceOf[AetherSchemeHandlerWorker]
+    worker
+  }
+
+  def localPath(path: String): String = s"phony:aether-dependencies-${Integer.toHexString(hashCode())}-${path}"
+
+  def resolve(path: String, targetContext: TargetContext) {
+
+    val requestedDeps = path.split(",").map { p => MavenGav(p) }
+
+    val files = worker.resolve(requestedDeps)
+
+    //    println("Resolved files: " + files)
+
+  }
+
+}
+
--- de.tototec.sbuild.experimental/src/main/scala/de/tototec/sbuild/experimental/aether/impl/AetherSchemeHandlerWorkerImpl.scala
+++ de.tototec.sbuild.experimental/src/main/scala/de/tototec/sbuild/experimental/aether/impl/AetherSchemeHandlerWorkerImpl.scala
@@ -1,13 +1,15 @@
-package de.tototec.sbuild.experimental
+package de.tototec.sbuild.experimental.aether.impl
 
 import java.io.File
+
+import scala.collection.JavaConverters.asScalaBufferConverter
+
 import org.apache.maven.repository.internal.MavenRepositorySystemUtils
 import org.apache.maven.wagon.Wagon
-import org.apache.maven.wagon.providers.file.FileWagon
-import org.apache.maven.wagon.providers.http.HttpWagon
 import org.eclipse.aether.RepositorySystem
 import org.eclipse.aether.artifact.DefaultArtifact
 import org.eclipse.aether.collection.CollectRequest
+import org.eclipse.aether.connector.file.FileRepositoryConnectorFactory
 import org.eclipse.aether.connector.wagon.WagonProvider
 import org.eclipse.aether.connector.wagon.WagonRepositoryConnectorFactory
 import org.eclipse.aether.graph.Dependency
@@ -16,17 +18,13 @@ import org.eclipse.aether.repository.RemoteRepository
 import org.eclipse.aether.resolution.DependencyRequest
 import org.eclipse.aether.spi.connector.RepositoryConnectorFactory
 import org.eclipse.aether.util.graph.visitor.PreorderNodeListGenerator
-import de.tototec.sbuild.MavenSupport.MavenGav
-import de.tototec.sbuild.SchemeResolver
-import de.tototec.sbuild.TargetContext
 import org.sonatype.maven.wagon.AhcWagon
-import scala.collection.JavaConverters._
-import org.eclipse.aether.connector.file.FileRepositoryConnectorFactory
 
-trait AetherContext {
-}
+import de.tototec.sbuild.MavenSupport.MavenGav
+import de.tototec.sbuild.experimental.aether.AetherSchemeHandler
+import de.tototec.sbuild.experimental.aether.AetherSchemeHandlerWorker
 
-object AetherSchemeHandler {
+object AetherSchemeHandlerWorkerImpl {
 
   class ManualWagonProvider extends WagonProvider {
     override def lookup(roleHint: String): Wagon = roleHint match {
@@ -41,43 +39,38 @@ object AetherSchemeHandler {
 
 }
 
-class AetherSchemeHandler(aetherContext: AetherContext = new AetherContext {}) extends SchemeResolver {
-  import AetherSchemeHandler._
+class AetherSchemeHandlerWorkerImpl(localRepoDir: File, remoteRepos: Seq[AetherSchemeHandler.Repository]) extends AetherSchemeHandlerWorker {
+  import AetherSchemeHandlerWorkerImpl._
 
-  def localPath(path: String): String = s"phony:maven-${path}-dependencies"
+  private[this] def newRepositorySystem() = {
+    val locator = MavenRepositorySystemUtils.newServiceLocator()
+    locator.setServices(classOf[WagonProvider], new ManualWagonProvider())
+    locator.setService(classOf[RepositoryConnectorFactory], classOf[FileRepositoryConnectorFactory])
+    locator.setService(classOf[RepositoryConnectorFactory], classOf[WagonRepositoryConnectorFactory])
+    val system = locator.getService(classOf[RepositorySystem])
+    assert(system != null)
+    system
+  }
 
-  def resolve(path: String, targetContext: TargetContext) {
+  private[this] def newSession(system: RepositorySystem) = {
+    val session = MavenRepositorySystemUtils.newSession()
+    val localRepo = new LocalRepository(localRepoDir)
+    session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, localRepo))
+    session
+  }
 
-    val localRepoDir = new File(System.getProperty("user.home") + "/.m2/repository")
+  lazy val repoSystem = newRepositorySystem()
+  lazy val session = newSession(repoSystem)
 
-    def newRepositorySystem() = {
-      val locator = MavenRepositorySystemUtils.newServiceLocator()
-      locator.setServices(classOf[WagonProvider], new ManualWagonProvider())
-      locator.setService(classOf[RepositoryConnectorFactory], classOf[FileRepositoryConnectorFactory])
-      locator.setService(classOf[RepositoryConnectorFactory], classOf[WagonRepositoryConnectorFactory])
-      val system = locator.getService(classOf[RepositorySystem])
-      assert(system != null)
-      system
-    }
+  override def resolve(requestedArtifacts: Seq[MavenGav]): Seq[File] = {
 
-    def newSession(system: RepositorySystem) = {
-      val session = MavenRepositorySystemUtils.newSession()
-      val localRepo = new LocalRepository(localRepoDir)
-      session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, localRepo))
-      session
-    }
-
-    val repoSystem = newRepositorySystem()
-    val session = newSession(repoSystem)
-
-    // somehow extract artifact info for given deps
-    val requestedArtifacts: Seq[MavenGav] = List()
     // create Maven dependencies from it
     val deps = requestedArtifacts.map {
-      case MavenGav(a, g, v, classifier) =>
-        new Dependency(new DefaultArtifact(s"$a:$g$v"), "compile")
+      case MavenGav(a, g, v, Some(c)) =>
+        new Dependency(new DefaultArtifact(s"$a:$g:jar:$c:$v"), "compile")
+      case MavenGav(a, g, v, None) =>
+        new Dependency(new DefaultArtifact(s"$a:$g:$v"), "compile")
     }
-    val dep = deps.head
 
     val centralRepo = new RemoteRepository.Builder("central", "default", "http://repo1.maven.org/maven2").build()
 
@@ -99,8 +92,8 @@ class AetherSchemeHandler(aetherContext: AetherContext = new AetherContext {}) e
     val nlg = new PreorderNodeListGenerator()
     node.accept(nlg)
 
-    val files = nlg.getNodes().asScala.map { n =>
-      val dep = n.getDependency()
+    val files = nlg.getNodes().asScala.toSeq.map { node =>
+      val dep = node.getDependency()
       val artifact = if (dep != null) dep.getArtifact() else null
       val file = if (artifact != null) artifact.getFile else null
       if (file != null) file.getAbsoluteFile() else null
@@ -109,7 +102,8 @@ class AetherSchemeHandler(aetherContext: AetherContext = new AetherContext {}) e
     //    val classpath = nlg.getClassPath()
     //    System.out.println(nlg.getClassPath())
 
-    println("Dependency files: " + files)
+    //    println("Dependency files: " + files)
+    files
 
   }
 
