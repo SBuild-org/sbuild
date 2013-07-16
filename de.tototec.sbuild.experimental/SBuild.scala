8f9369bd1af60d6d836839d718f93226ebbf57d9
Tobias Roeser <le.petit.fou@web.de>
2013-07-16 21:29:10 +0000

    Build aether jars.

--- de.tototec.sbuild.experimental/SBuild.scala
+++ de.tototec.sbuild.experimental/SBuild.scala
@@ -8,7 +8,7 @@ import de.tototec.sbuild.TargetRefs._
 @classpath("mvn:org.apache.ant:ant:1.8.4")
 class SBuild(implicit _project: Project) {
 
-  import SBuildConfig.{sbuildVersion,compilerPath,scalaVersion}
+  import SBuildConfig.{ sbuildVersion, compilerPath, scalaVersion }
 
   val namespace = "de.tototec.sbuild.experimental"
   val jar = s"target/${namespace}-${sbuildVersion}.jar"
@@ -18,41 +18,41 @@ class SBuild(implicit _project: Project) {
   val wagonVersion = "2.4"
   val aetherCp =
     s"mvn:org.eclipse.aether:aether-api:${aetherVersion}" ~
-    s"mvn:org.eclipse.aether:aether-spi:${aetherVersion}" ~
-    s"mvn:org.eclipse.aether:aether-util:${aetherVersion}" ~
-    s"mvn:org.eclipse.aether:aether-impl:${aetherVersion}" ~
-    s"mvn:org.eclipse.aether:aether-connector-file:${aetherVersion}" ~
-    s"mvn:org.eclipse.aether:aether-connector-asynchttpclient:${aetherVersion}" ~
-    s"mvn:org.eclipse.aether:aether-connector-wagon:${aetherVersion}" ~
-    "mvn:io.tesla.maven:maven-aether-provider:3.1.2" ~
-    s"mvn:org.apache.maven.wagon:wagon-provider-api:${wagonVersion}" ~
-    s"mvn:org.apache.maven.wagon:wagon-http:${wagonVersion}" ~
-    s"mvn:org.apache.maven.wagon:wagon-file:${wagonVersion}" ~
-    s"mvn:org.apache.maven.wagon:wagon-ssh:${wagonVersion}" ~
-    "mvn:org.sonatype.maven:wagon-ahc:1.2.1" ~
-    s"mvn:org.apache.maven.wagon:wagon-http-shared4:${wagonVersion}" ~
-    s"mvn:org.codehaus.plexus:plexus-component-annotations:1.5.5" ~
-    s"mvn:org.apache.httpcomponents:httpclient:4.2.5" ~
-    s"mvn:org.apache.httpcomponents:httpcore:4.2.4" ~
-    "mvn:javax.inject:javax.inject:1" ~
-    "mvn:com.ning:async-http-client:1.6.5" ~
-    "mvn:io.tesla.maven:maven-model:3.1.0" ~
-    "mvn:io.tesla.maven:maven-model-builder:3.1.0" ~
-    "mvn:io.tesla.maven:maven-repository-metadata:3.1.0" ~
-    "mvn:org.jboss.netty:netty:3.2.5.Final" ~
-    "mvn:org.eclipse.sisu:org.eclipse.sisu.inject:0.0.0.M1" ~
-    "mvn:org.eclipse.sisu:org.eclipse.sisu.plexus:0.0.0.M1" ~
-    "mvn:org.codehaus.plexus:plexus-classworlds:2.4" ~
-    "mvn:org.codehaus.plexus:plexus-interpolation:1.16" ~
-    "mvn:org.codehaus.plexus:plexus-utils:2.1" ~
-    "mvn:org.sonatype.sisu:sisu-guava:0.9.9" ~
-    "mvn:org.sonatype.sisu:sisu-guice:3.1.0" ~
-    "mvn:org.slf4j:slf4j-api:1.6.2"
+      s"mvn:org.eclipse.aether:aether-spi:${aetherVersion}" ~
+      s"mvn:org.eclipse.aether:aether-util:${aetherVersion}" ~
+      s"mvn:org.eclipse.aether:aether-impl:${aetherVersion}" ~
+      s"mvn:org.eclipse.aether:aether-connector-file:${aetherVersion}" ~
+      s"mvn:org.eclipse.aether:aether-connector-asynchttpclient:${aetherVersion}" ~
+      s"mvn:org.eclipse.aether:aether-connector-wagon:${aetherVersion}" ~
+      "mvn:io.tesla.maven:maven-aether-provider:3.1.2" ~
+      s"mvn:org.apache.maven.wagon:wagon-provider-api:${wagonVersion}" ~
+      s"mvn:org.apache.maven.wagon:wagon-http:${wagonVersion}" ~
+      s"mvn:org.apache.maven.wagon:wagon-file:${wagonVersion}" ~
+      s"mvn:org.apache.maven.wagon:wagon-ssh:${wagonVersion}" ~
+      "mvn:org.sonatype.maven:wagon-ahc:1.2.1" ~
+      s"mvn:org.apache.maven.wagon:wagon-http-shared4:${wagonVersion}" ~
+      s"mvn:org.codehaus.plexus:plexus-component-annotations:1.5.5" ~
+      s"mvn:org.apache.httpcomponents:httpclient:4.2.5" ~
+      s"mvn:org.apache.httpcomponents:httpcore:4.2.4" ~
+      "mvn:javax.inject:javax.inject:1" ~
+      "mvn:com.ning:async-http-client:1.6.5" ~
+      "mvn:io.tesla.maven:maven-model:3.1.0" ~
+      "mvn:io.tesla.maven:maven-model-builder:3.1.0" ~
+      "mvn:io.tesla.maven:maven-repository-metadata:3.1.0" ~
+      "mvn:org.jboss.netty:netty:3.2.5.Final" ~
+      "mvn:org.eclipse.sisu:org.eclipse.sisu.inject:0.0.0.M1" ~
+      "mvn:org.eclipse.sisu:org.eclipse.sisu.plexus:0.0.0.M1" ~
+      "mvn:org.codehaus.plexus:plexus-classworlds:2.4" ~
+      "mvn:org.codehaus.plexus:plexus-interpolation:1.16" ~
+      "mvn:org.codehaus.plexus:plexus-utils:2.1" ~
+      "mvn:org.sonatype.sisu:sisu-guava:0.9.9" ~
+      "mvn:org.sonatype.sisu:sisu-guice:3.1.0" ~
+      "mvn:org.slf4j:slf4j-api:1.6.2"
 
   val compileCp =
     s"mvn:org.scala-lang:scala-library:${scalaVersion}" ~
-    s"../de.tototec.sbuild/target/de.tototec.sbuild-${sbuildVersion}.jar" ~
-    aetherCp
+      s"../de.tototec.sbuild/target/de.tototec.sbuild-${sbuildVersion}.jar" ~
+      aetherCp
 
   ExportDependencies("eclipse.classpath", compileCp)
 
@@ -98,5 +98,25 @@ class SBuild(implicit _project: Project) {
       docTitle = s"SBuild Experimental API Reference"
     )
   }
+
+  Target(s"target/de.tototec.sbuild.experimental.aether-${sbuildVersion}.jar") dependsOn
+    "compile" ~ "LICENSE.txt" exec { ctx: TargetContext =>
+      AntJar(
+        destFile = ctx.targetFile.get,
+        baseDir = Path("target/classes"),
+        includes = "de/tototec/sbuild/experimental/aether/*.class",
+        fileSet = AntFileSet(file = Path("LICENSE.txt"))
+      )
+    }
+
+  Target(s"target/de.tototec.sbuild.experimental.aether.impl-${sbuildVersion}.jar") dependsOn
+    "compile" ~ "LICENSE.txt" exec { ctx: TargetContext =>
+      AntJar(
+        destFile = ctx.targetFile.get,
+        baseDir = Path("target/classes"),
+        includes = "de/tototec/sbuild/experimental/aether/impl/*.class",
+        fileSet = AntFileSet(file = Path("LICENSE.txt"))
+      )
+    }
 
 }
