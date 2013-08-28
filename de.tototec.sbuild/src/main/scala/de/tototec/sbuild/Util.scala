946779e94d800b24fcd99db16042632ed825c477
Tobias Roeser <le.petit.fou@web.de>
2013-08-28 21:32:31 +0000

    Better download failure handling (resume, retry).

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/Util.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/Util.scala
@@ -40,6 +40,8 @@ object Util {
 
   def download(url: String, target: String, log: SBuildLogger = log, userAgent: Option[String]): Option[Throwable] = {
 
+    val retryCount = 5
+
     try {
 
       val targetFile = new File(target)
@@ -66,59 +68,95 @@ object Util {
 
           val outStream = new BufferedOutputStream(new FileOutputStream(downloadTargetFile))
           try {
-            val connection = new URL(url).openConnection
-            userAgent.map { agent => connection.setRequestProperty("User-Agent", agent) }
-            val inStream = new BufferedInputStream(connection.getInputStream())
 
-            // connection opened
-            val contentLength = connection.getHeaderField("content-length") match {
-              case null => -1
-              case length => try { length.toLong } catch { case _: Exception => -1 }
-            }
-
-            var last = System.currentTimeMillis
+            var lastContentLength: Option[Long] = None
             var len = 0
-            var break = false
-            var alreadyLogged = false
+            var lastRetryLen = 0
+            var retry = true
+            var retries = 0
 
-            val format = new DecimalFormat("#,##0.#")
-            def formatLength(length: Long): String = format.format(length / 1024)
+            while (retry) {
+              retry = false
 
-            def logProgress = if (contentLength > 0) {
-              log.log(LogLevel.Info, s"Downloaded ${formatLength(len)} of ${formatLength(contentLength)} kb (${format.format((len * 1000 / contentLength).toLong.toDouble / 10)}%) from ${url}")
-            } else {
-              log.log(LogLevel.Info, s"Downloaded ${formatLength(len)} kb from ${url}")
-            }
-
-            var buffer = new Array[Byte](1024)
-
-            while (!break) {
-              val now = System.currentTimeMillis
-              if (len > 0 && now > last + 5000) {
-                alreadyLogged = true
-                logProgress
-                last = now;
+              val connection = new URL(url).openConnection
+              userAgent.map { agent => connection.setRequestProperty("User-Agent", agent) }
+              if (len > 0) {
+                // TODO: also check http header Accept-Ranges
+                connection.setRequestProperty("Range", s"bytes=${len}-")
               }
-              inStream.read(buffer, 0, 1024) match {
-                case x if x < 0 => break = true
-                case count => {
-                  len = len + count
-                  outStream.write(buffer, 0, count)
+              val inStream = new BufferedInputStream(connection.getInputStream())
+
+              // connection opened
+              val contentLength = lastContentLength.getOrElse {
+                val cl = connection.getHeaderField("content-length") match {
+                  case null => -1
+                  case length => try { length.toLong } catch { case _: Exception => -1 }
+                }
+                lastContentLength = Some(cl)
+                cl
+              }
+
+              var last = System.currentTimeMillis
+              var break = false
+              var alreadyLogged = false
+
+              val format = new DecimalFormat("#,##0.#")
+              def formatLength(length: Long): String = format.format(length / 1024)
+
+              def logProgress = if (contentLength > 0) {
+                log.log(LogLevel.Info, s"Downloaded ${formatLength(len)} of ${formatLength(contentLength)} kb (${format.format((len.toDouble * 1000 / contentLength.toDouble).toLong.toDouble / 10)}%) from ${url}")
+              } else {
+                log.log(LogLevel.Info, s"Downloaded ${formatLength(len)} kb from ${url}")
+              }
+
+              var buffer = new Array[Byte](1024)
+
+              while (!break) {
+                val now = System.currentTimeMillis
+                if (len > 0 && now > last + 5000) {
+                  alreadyLogged = true
+                  logProgress
+                  last = now;
+                }
+                inStream.read(buffer, 0, 1024) match {
+                  case x if x < 0 => break = true
+                  case count => {
+                    len = len + count
+                    outStream.write(buffer, 0, count)
+                  }
                 }
               }
+
+              if (alreadyLogged && len > 0) {
+                logProgress
+              }
+
+              if (contentLength >= 0 && len != contentLength) {
+                log.log(LogLevel.Info, s"Download stream closed before download was complete from ${url}")
+
+                if (retries < retryCount) {
+                  retry = true
+                  alreadyLogged = true
+                  retries = if (len > lastRetryLen) 0 else (retries + 1)
+                  lastRetryLen = len
+                  log.log(LogLevel.Info, s"Resuming download from ${url}")
+
+                } else {
+                  outStream.close
+                  cleanup
+                  throw new SBuildException(s"To many failed retries (s${retries}). Cannot download from ${url}");
+                }
+
+                // stream closed to early
+                // TODO: resume download, if connection is closed before complete file was transfered
+                // TODO: if no resume is supported, retry n times before giving up
+                // TODO: implement a timeout, to avoid ever-blocking downloads
+              }
+
+              inStream.close
+
             }
 
-            if (alreadyLogged && len > 0) {
-              logProgress
-            }
-
-            if (contentLength > 0 && len != contentLength) {
-              // stream closed to early
-              // TODO: resume download, if connection is closed before complete file was transfered
-              // TODO: if no resume is supported, retry n times before giving up
-            }
-
-            inStream.close
           } catch {
             case e: FileNotFoundException =>
               outStream.close
