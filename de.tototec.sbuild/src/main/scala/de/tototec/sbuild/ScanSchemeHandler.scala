7e82ca043f175ce163cb55a20bf02b4846ceaebb
Tobias Roeser <le.petit.fou@web.de>
2013-09-03 15:24:49 +0000

    Improved error message when wrong syntax was used in ScanSchemeHandler.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/ScanSchemeHandler.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/ScanSchemeHandler.scala
@@ -23,25 +23,20 @@ class ScanSchemeHandler(implicit project: Project)
   override def resolve(schemeCtx: SchemeContext, targetContext: TargetContext): Unit = {
     // Ensure, we always report a last modified, even, if we don't find any file
     targetContext.targetLastModified = 1
-    scan(schemeCtx.path, targetContext).foreach { f => targetContext.attachFile(f) }
+    scan(schemeCtx.scheme, schemeCtx.path, targetContext).foreach { f => targetContext.attachFile(f) }
   }
 
-  def scan(path: String, targetContext: TargetContext): Array[File] = {
+  protected def scan(scheme: String, path: String, targetContext: TargetContext): Array[File] = {
     // TODO: parse path pattern, for now, only a simple regex
     // path := dir [ ';' 'asDependencies' ] [ ';' 'regex=' regex ]
 
-    val nPath =
-      if (path.contains(";asDependencies"))
-        path.replaceFirst(";asDependencies", "")
-      else path
-
-    val (dir, regex) = nPath.split(";", 2) match {
+    val (dir, regex) = path.split(";", 2) match {
       case Array(dir) =>
         (Path(dir), ".*".r)
       case Array(dir, regex) if regex.startsWith("regex=") =>
         (Path(dir), regex.substring(6).r)
       case _ =>
-        val ex = new ProjectConfigurationException("Unsupported syntax used in target/depenency: " + targetContext.name)
+        val ex = new ProjectConfigurationException(s"""Unsupported syntax used in target/depenency "${targetContext.name}". Syntax: '${scheme}:' dir [';regex=' regex ]""")
         ex.buildScript = Some(targetContext.project.projectFile)
         throw ex
     }
