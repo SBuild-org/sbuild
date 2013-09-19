910065ee1c8331b12eba0c8411b11a730fac087f
Tobias Roeser <le.petit.fou@web.de>
2013-09-19 08:38:54 +0000

    Added guard to ResolveFiles, checking that it is not executed within an target.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/ResolveFiles.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/ResolveFiles.scala
@@ -10,36 +10,45 @@ object ResolveFiles {
 
   def apply(targetRefs: TargetRefs)(implicit project: Project): Seq[File] = {
 
-    lazy val targetExecutor = new TargetExecutor(project, project.log, logConfig = TargetExecutor.LogConfig(topLevelSkipped = LogLevel.Debug))
+    WithinTargetExecution.get match {
+      case null =>
+        // ensure, we don't get executed within an target
+        lazy val targetExecutor = new TargetExecutor(project, project.log, logConfig = TargetExecutor.LogConfig(topLevelSkipped = LogLevel.Debug))
 
-    project.log.log(LogLevel.Debug, "ResolveFiles request: " + targetRefs)
+        project.log.log(LogLevel.Debug, "ResolveFiles request: " + targetRefs)
 
-    val targetRefFiles = targetRefs.targetRefs.flatMap { targetRef =>
-      project.determineRequestedTarget(targetRef, searchInAllProjects = true, supportCamelCaseShortCuts = false) match {
-        case None =>
-          // not found
-          // if an existing file, then proceed.
-          targetRef.explicitProto match {
-            case None | Some("file") if targetRef.explicitProject == None && Path(targetRef.nameWithoutProto).exists =>
-              val fileRef = Path(targetRef.nameWithoutProto)
-              project.log.log(LogLevel.Debug, "Resolved to a local file reference: " + fileRef)
-              Seq(fileRef)
-            case _ =>
-              throw new TargetNotFoundException(s"""Could not found target with name "${targetRef}" in project ${project.projectFile}.""")
+        val targetRefFiles = targetRefs.targetRefs.flatMap { targetRef =>
+          project.determineRequestedTarget(targetRef, searchInAllProjects = true, supportCamelCaseShortCuts = false) match {
+            case None =>
+              // not found
+              // if an existing file, then proceed.
+              targetRef.explicitProto match {
+                case None | Some("file") if targetRef.explicitProject == None && Path(targetRef.nameWithoutProto).exists =>
+                  val fileRef = Path(targetRef.nameWithoutProto)
+                  project.log.log(LogLevel.Debug, "Resolved to a local file reference: " + fileRef)
+                  Seq(fileRef)
+                case _ =>
+                  throw new TargetNotFoundException(s"""Could not found target with name "${targetRef}" in project ${project.projectFile}.""")
+              }
+
+            case Some(target) =>
+              project.log.log(LogLevel.Debug, "About to resolve target: " + target)
+              val executedTarget = targetExecutor.preorderedDependenciesTree(curTarget = target)
+
+              project.log.log(LogLevel.Debug, "Resolved target '" + target + "' to: " + executedTarget)
+              executedTarget.targetContext.targetFiles
+
           }
+        }
 
-        case Some(target) =>
-          project.log.log(LogLevel.Debug, "About to resolve target: " + target)
-          val executedTarget = targetExecutor.preorderedDependenciesTree(curTarget = target)
+        project.log.log(LogLevel.Debug, "Resolved files:\n  - " + targetRefFiles.mkString("\n  - "))
 
-          project.log.log(LogLevel.Debug, "Resolved target '" + target + "' to: " + executedTarget)
-          executedTarget.targetContext.targetFiles
+        targetRefFiles
 
-      }
+      case _ =>
+        val ex = InvalidApiUsageException.localized("'ResolveFiles' can only be used outside an exec block of a target.")
+        ex.buildScript = Some(project.projectFile)
+        throw ex
     }
-
-    project.log.log(LogLevel.Debug, "Resolved files:\n  - " + targetRefFiles.mkString("\n  - "))
-
-    targetRefFiles
   }
 }
