b5fe47ed65a02d3ec3b6175642ea5d88ff548510
Tobias Roeser <le.petit.fou@web.de>
2013-09-20 10:24:59 +0000

    Added root page to scaladoc. Also use Unix4j to substitute variables (versions) in scaladoc sources.

--- de.tototec.sbuild.addons/SBuild.scala
+++ de.tototec.sbuild.addons/SBuild.scala
@@ -7,14 +7,17 @@ import de.tototec.sbuild.ant.tasks._
 @include("../SBuildConfig.scala",
   "src/main/scala/de/tototec/sbuild/addons/java/Javadoc.scala"
 )
-@classpath("mvn:org.apache.ant:ant:1.8.4")
+@classpath("mvn:org.apache.ant:ant:1.8.4",
+  "mvn:org.unix4j:unix4j-base:0.3",
+  "mvn:org.unix4j:unix4j-command:0.3"
+)
 class SBuild(implicit _project: Project) {
 
   val addonsJar = s"target/de.tototec.sbuild.addons-${SBuildConfig.sbuildVersion}.jar"
 
   val compileCp =
     SBuildConfig.scalaLibrary ~
-    s"../de.tototec.sbuild/target/de.tototec.sbuild-${SBuildConfig.sbuildVersion}.jar"
+      s"../de.tototec.sbuild/target/de.tototec.sbuild-${SBuildConfig.sbuildVersion}.jar"
   //      ("mvn: org.scalatest:scalatest_" + scalaVersion + ":1.6.1") ~
   //      ("http://cloud.github.com/downloads/KentBeck/junit/junit-4.10.jar") ~
   //      ("http://cloud.github.com/downloads/KentBeck/junit/junit-4.10-src.jar")
@@ -43,49 +46,73 @@ class SBuild(implicit _project: Project) {
 
   Target(addonsJar) dependsOn ("compile") exec { ctx: TargetContext =>
     AntJar(
-      destFile = ctx.targetFile.get, 
+      destFile = ctx.targetFile.get,
       baseDir = Path("target/classes"),
       fileSet = AntFileSet(dir = Path("."), includes = "LICENSE.txt")
     )
   }
 
-  Target("phony:scaladoc").cacheable dependsOn SBuildConfig.compilerPath ~ compileCp ~ "scan:src/main/scala" exec {
-    addons.scala.Scaladoc(
-      scaladocClasspath = SBuildConfig.compilerPath.files,
-      classpath = compileCp.files,
-      sources = "scan:src/main/scala".files,
-      destDir = Path("target/scaladoc"),
-      deprecation = true, unchecked = true, implicits = true,
-      docVersion = SBuildConfig.sbuildVersion,
-      docTitle = s"SBuild Addons Reference"
-    )
-  }
+  Target("phony:scaladoc").cacheable dependsOn SBuildConfig.compilerPath ~ compileCp ~~
+    "scan:src/main/scala" ~ "src/main/scaladoc/root.tracwiki" exec {
+
+      import java.io.File
+
+      def substVars(source: File, target: File) = {
+        target.getParentFile.mkdirs
+        org.unix4j.Unix4j.cat(source).sed(s"s/\\$$#\\{SBUILD_VERSION\\}/${SBuildConfig.sbuildVersion}/g").toFile(target)
+      }
+
+      // parse root page
+      val targetRoot = Path("target/generated-scaladoc/root.tracwiki")
+      substVars(Path("src/main/scaladoc/root.tracwiki"), targetRoot)
+
+      def substVarsInDir(sourceDir: File, targetDir: File, files: Seq[File]) = {
+        val prefixL = sourceDir.getPath.length
+        files.foreach { file =>
+          val relFile = file.getPath.substring(prefixL)
+          val targetFile = Path(targetDir, relFile)
+          substVars(file, targetFile)
+        }
+      }
+      substVarsInDir(Path("src/main/scala"), Path("target/generated-scaladoc"), "scan:src/main/scala".files)
+
+      addons.scala.Scaladoc(
+        scaladocClasspath = SBuildConfig.compilerPath.files,
+        classpath = compileCp.files,
+        srcDir = Path("target/generated-scaladoc"),
+        destDir = Path("target/scaladoc"),
+        deprecation = true, unchecked = true, implicits = true,
+        docVersion = SBuildConfig.sbuildVersion,
+        docTitle = s"SBuild Addons Reference",
+        additionalScaladocArgs = Seq("-doc-root-content", targetRoot.getPath)
+      )
+    }
 
   val genJavaDoc = s"mvn:com.typesafe.genjavadoc:genjavadoc-plugin_${SBuildConfig.scalaVersion}:0.4"
 
   Target("phony:genjavadoc").cacheable dependsOn
     SBuildConfig.compilerPath ~ genJavaDoc ~ compileCp ~ "scan:src/main/scala" exec {
-    addons.scala.Scalac(
-      compilerClasspath = SBuildConfig.compilerPath.files,
-      classpath = compileCp.files,
-      sources = "scan:src/main/scala".files,
-      destDir = Path("target/genjavadoc-classes"),
-      unchecked = true, deprecation = true, debugInfo = "vars",
-      additionalScalacArgs = Seq(
-        s"""-Xplugin:${genJavaDoc.files.head.getPath}""",
-        s"""-P:genjavadoc:out=${Path("target/genjavadoc")}"""
+      addons.scala.Scalac(
+        compilerClasspath = SBuildConfig.compilerPath.files,
+        classpath = compileCp.files,
+        sources = "scan:src/main/scala".files,
+        destDir = Path("target/genjavadoc-classes"),
+        unchecked = true, deprecation = true, debugInfo = "vars",
+        additionalScalacArgs = Seq(
+          s"""-Xplugin:${genJavaDoc.files.head.getPath}""",
+          s"""-P:genjavadoc:out=${Path("target/genjavadoc")}"""
+        )
       )
-    )
-  }
+    }
 
   Target("phony:javadoc").cacheable dependsOn "genjavadoc" ~
     SBuildConfig.compilerPath ~ compileCp ~ "scan:target/genjavadoc" exec {
-    addons.java.Javadoc(
-      classpath = compileCp.files,
-      sources = "scan:target/genjavadoc".files,
-      destDir = Path("target/javadoc"),
-      fork = true
-    )
-  }
+      addons.java.Javadoc(
+        classpath = compileCp.files,
+        sources = "scan:target/genjavadoc".files,
+        destDir = Path("target/javadoc"),
+        fork = true
+      )
+    }
 
 }
--- de.tototec.sbuild.addons/src/main/scala/de/tototec/sbuild/addons/package.scala
+++ de.tototec.sbuild.addons/src/main/scala/de/tototec/sbuild/addons/package.scala
@@ -1,16 +1,17 @@
 package de.tototec.sbuild
 
 /**
+ *
  * ==SBuild Addons Reference==
- * 
+ *
  * This is the API Reference for SBuild Addons.
- * 
- * This version is designed to be used with SBuild 0.4.0.
- * 
+ *
+ * This version is designed to be used with SBuild $#{SBUILD_VERSION}.
+ *
  * Project Homepage: [[http://sbuild.tototec.de/]]
- * 
+ *
  * SBuild Addons are released under the [[http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0]].
- * 
+ *
  */
 package object addons {
 
