1abf53c506b5516b4246aebc3f8cab5025c08fb4
Tobias Roeser <le.petit.fou@web.de>
2013-10-17 12:14:18 +0000

    Added a logging bridge, delegating to SLF4J if available.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/Logger.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/Logger.scala
@@ -1,5 +1,60 @@
 package de.tototec.sbuild
 
+trait Logger extends Serializable {
+  def error(msg: => String, throwable: Throwable = null)
+  def warn(msg: => String, throwable: Throwable = null)
+  def info(msg: => String, throwable: Throwable = null)
+  def debug(msg: => String, throwable: Throwable = null)
+  def trace(msg: => String, throwable: Throwable = null)
+}
+
+object Logger {
+  import scala.reflect.ClassTag
+  import scala.reflect.classTag
+  import scala.util.Success
+  import scala.util.Try
+  import java.lang.reflect.Method
+
+  private[this] var cachedLoggerFactory: Option[String => Logger] = None
+  def apply[T: ClassTag]: Logger = cachedLoggerFactory match {
+    case Some(loggerFactory) => loggerFactory(classTag[T].runtimeClass.getName)
+    case None =>
+      // try to load SLF4J
+      def delegatedLoadingOfLoggerFactory: String => Logger = {
+        import org.slf4j.LoggerFactory
+        LoggerFactory.getILoggerFactory()
+
+        // if we are here, loading the LoggerFactory was successful
+        loggedClass => new Logger {
+          private[this] val underlying = LoggerFactory.getLogger(loggedClass)
+          override def error(msg: => String, throwable: Throwable) = if (underlying.isErrorEnabled) underlying.error(msg, throwable)
+          override def warn(msg: => String, throwable: Throwable) = if (underlying.isWarnEnabled) underlying.warn(msg, throwable)
+          override def info(msg: => String, throwable: Throwable) = if (underlying.isInfoEnabled) underlying.info(msg, throwable)
+          override def debug(msg: => String, throwable: Throwable) = if (underlying.isDebugEnabled) underlying.debug(msg, throwable)
+          override def trace(msg: => String, throwable: Throwable) = if (underlying.isTraceEnabled) underlying.trace(msg, throwable)
+        }
+      }
+
+      val loggerFactory = try {
+        delegatedLoadingOfLoggerFactory
+      } catch {
+        case e: NoClassDefFoundError =>
+          _: String => new Logger {
+            override def error(msg: => String, throwable: Throwable) {}
+            override def warn(msg: => String, throwable: Throwable) {}
+            override def info(msg: => String, throwable: Throwable) {}
+            override def debug(msg: => String, throwable: Throwable) {}
+            override def trace(msg: => String, throwable: Throwable) {}
+          }
+      }
+
+      cachedLoggerFactory = Some(loggerFactory)
+
+      // retry, now initialized
+      apply[T]
+  }
+}
+
 trait SBuildLogger {
   def log(logLevel: LogLevel, msg: => String, cause: Throwable = null)
 }
