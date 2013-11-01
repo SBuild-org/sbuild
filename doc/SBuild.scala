b5aea46a614bb237f035003f38f088f2cfe88d41
Tobias Roeser <le.petit.fou@web.de>
2013-11-01 16:33:50 +0000

    Started to work on documentation again, this time with a AsciiDoc toolchain. AsciidoctorJ does not work as expected for now, so a native asciidoc tool is used.

--- doc/SBuild.scala
+++ doc/SBuild.scala
@@ -7,190 +7,17 @@ import de.tototec.sbuild.TargetRefs._
 @include("../SBuildConfig.scala")
 @classpath(
   "mvn:org.apache.ant:ant:1.8.4",
-  "zip:file=plugins/org.eclipse.mylyn.wikitext.core_1.7.2.v20120916-1200.jar;archive=http://mirror.netcologne.de/eclipse//mylyn/drops/3.8.2/v20120916-1200/mylyn-3.8.2.v20120916-1200.zip",
-  "zip:file=plugins/org.eclipse.mylyn.wikitext.textile.core_1.7.2.v20120916-1200.jar;archive=http://mirror.netcologne.de/eclipse//mylyn/drops/3.8.2/v20120916-1200/mylyn-3.8.2.v20120916-1200.zip"
+  "mvn:org.asciidoctor:asciidoctor-java-integration:0.1.4",
+  "mvn:org.jruby:jruby-complete:1.7.4"
 )
 class SBuild(implicit _project: Project) {
 
-  Target("phony:clean") exec {
+  Target("phony:clean").evictCache exec {
     AntDelete(dir = Path("target"))
+    // Path("target").deleteRecursive
   }
 
-  Target("phony:all") dependsOn "convert-wiki-to-html" ~ "convert-manual-to-html" ~ "convert-manual-to-docbook"
-
-  // TODO: depend on wiki files
-  Target("phony:convert-wiki-to-html") exec {
-    AntCopy(fileSet = AntFileSet(dir = Path("wiki")), toDir = Path("target/wiki"))
-
-    // Preprocess wiki files converting internal links to external ones.
-    val files = Path("target/wiki").listFiles.filter(f => f.getName.endsWith(".textile"))
-    resolveTextile(files = files, includeSearchPath = Seq(Path("wiki")), linkPattern = "{0}.html")
-
-    // Convert wiki files
-    new org.eclipse.mylyn.wikitext.core.util.anttask.MarkupToHtmlTask() {
-      setProject(AntProject())
-      setMarkupLanguage("Textile")
-      addFileset(AntFileSet(dir = Path("target/wiki")))
-      setSourceEncoding("UTF-8")
-    }.execute
-
-  }
-
-  // TODO: depend on wiki files
-  Target("phony:convert-manual-to-html") exec {
-    AntCopy(fileSet = AntFileSet(dir = Path("manual")), toDir = Path("target/manual"))
-
-    // Preprocess wiki files converting internal links to external ones.
-    val files = Path("target/manual").listFiles.filter(f => f.getName.endsWith(".textile"))
-    resolveTextile(files = files, includeSearchPath = Seq(Path("manual")), linkPattern = "{0}.html")
-
-    val stylesheet = new org.eclipse.mylyn.wikitext.core.util.anttask.MarkupToHtmlTask.Stylesheet()
-    //    stylesheet.setStylesheet(readFile(Path("manual/style.css")))
-    stylesheet.setFile(Path("manual/style.css"))
-
-    // Convert wiki files
-    new org.eclipse.mylyn.wikitext.core.util.anttask.MarkupToHtmlTask() {
-      setProject(AntProject())
-      setMarkupLanguage("Textile")
-      addFileset(AntFileSet(dir = Path("target/manual")))
-      setSourceEncoding("UTF-8")
-      addStylesheet(stylesheet)
-    }.execute
-
-  }
-
-  // TODO: depend on wiki files
-  Target("phony:convert-manual-to-docbook") exec {
-    AntCopy(fileSet = AntFileSet(dir = Path("manual")), toDir = Path("target/manual"))
-
-    // Preprocess wiki files converting internal links to external ones.
-    val files = Path("target/manual").listFiles.filter(f => f.getName.endsWith(".textile"))
-    resolveTextile(files = files, includeSearchPath = Seq(Path("manual")), linkPattern = "{0}.html")
-
-    // Convert wiki files
-    new org.eclipse.mylyn.wikitext.core.util.anttask.MarkupToDocbookTask() {
-      setProject(AntProject())
-      setMarkupLanguage("Textile")
-      //      addFileset(AntFileSet(file = Path("target/manual")))
-      setFile(Path("target/manual/ReferenceManual.textile"))
-      setSourceEncoding("UTF-8")
-    }.execute
-
-  }
-
-  Target("phony:convert-docbook-to-html") dependsOn "convert-manual-to-docbook" exec {
-    new org.apache.tools.ant.taskdefs.XSLTProcess() {
-      setProject(AntProject())
-      setIn(Path("target/manual/ReferenceManual.xml"))
-      setExtension("xml")
-      setOut(Path("target/manual/ReferenceManual.fo"))
-
-    }.execute
-  }
-
-  // Helper for textile
-
-  def resolveTextile(files: Seq[java.io.File], includeSearchPath: Seq[java.io.File], linkPattern: String) = {
-    val link = new java.text.MessageFormat(linkPattern)
-    files.foreach { file =>
-      println("Resolving textile file: " + file)
-
-      val fileName = file.getName match {
-        case n if n.toLowerCase.endsWith(".textile") =>
-          n.substring(0, n.length - 8)
-        case n => n
-      }
-
-      var content = readFile(file)
-
-      val includes = resolveIncludes(content, includeSearchPath)
-      content = includes._1
-      val names = includes._2
-
-      content = resolveLocalLinks(content, linkPattern, names.map(name => name -> fileName).toMap)
-
-      val writer = new java.io.FileWriter(file)
-      writer.write(content)
-      writer.close
-    }
-  }
-
-  val internalLinkWithName = """\[\[([^|#]+?)(#.*?)([|](.+?))?\]\]""".r
-
-  // TODO: handle #-refs
-  def resolveLocalLinks(content: String, linkPattern: String, aliasMap: Map[String, String]): String = {
-    val link = new java.text.MessageFormat(linkPattern)
-
-    def target(name: String): String = aliasMap.get(name) match {
-      case None => name
-      case Some(name) => name
-    }
-
-    internalLinkWithName.replaceAllIn(content, m => {
-      val mLink = m.group(1)
-      val mInnerRef = m.group(2) match {
-        case ref if ref != null && ref.trim.length > 1 => ref
-        case _ => ""
-      }
-      val mTitle = m.group(4) match {
-        case t if t != null && t.trim.length > 0 => t
-        case _ => mLink
-      }
-
-      java.util.regex.Matcher.quoteReplacement(
-        s""""${mTitle}":${link.format(Array(target(mLink)))}${mInnerRef}"""
-      )
-
-    })
-  }
-
-  def readFile(file: java.io.File): String = {
-    if (!file.exists) throw new java.io.FileNotFoundException("Could not found file: " + file)
-    val source = scala.io.Source.fromFile(file)
-    val content = source.getLines.mkString("\n")
-    source.close
-    content
-  }
-
-  val include = """\{\{include\((.+?)\)\}\}""".r
-
-  // TODO: replace links to included-files into self-links
-  /**
-   * @return A Pair _1: containing the included content, _2: a map containing included file names
-   */
-  def resolveIncludes(content: String, searchPath: Seq[java.io.File]): (String, Seq[String]) = {
-    var names: Seq[String] = Seq()
-
-    val newContent = include.replaceAllIn(content, m => {
-      val name = m.group(1)
-      names ++= Seq(name)
-      val part = name + ".textile"
-      println("Including document: " + part)
-
-      var includePath = searchPath.find {
-        path => Path(path.getPath, part).exists
-      }.map {
-        path => Path(path.getPath, part)
-      }
-
-      includePath match {
-        case None =>
-          throw new java.io.FileNotFoundException("Could not found file: " + part)
-        case Some(file) =>
-          val included = readFile(file)
-          val result = if (include.findFirstIn(included).isDefined) {
-            val (newContent, newNames) = resolveIncludes(included, searchPath)
-            names ++= newNames
-            newContent
-          } else {
-            included
-          }
-          java.util.regex.Matcher.quoteReplacement(result)
-      }
-    })
-
-    (newContent, names)
-  }
+  Target("phony:all") dependsOn "manpage" ~ "manual"
 
   val jBakeDeps = Seq(
     "jbake.jar",
@@ -209,6 +36,156 @@ class SBuild(implicit _project: Project) {
     addons.support.ForkSupport.runJavaAndWait(classpath = jBakeDeps.files, arguments = Array(
       "org.jbake.launcher.Main", sourceDir.getAbsolutePath, targetDir.getAbsolutePath
     ))
+  }
+
+  val asciidoctor = "mvn:org.asciidoctor:asciidoctor-java-integration:0.1.4"
+  val jruby = "mvn:org.jruby:jruby-complete:1.7.4"
+  val manual = "src/manual/SBuildManual.adoc"
+  val manpage = "src/manpage/sbuild.1.adoc"
+
+  Target("phony:manpage") dependsOn "manpage-native"
+
+  Target("phony:manpage-asciidoctor-fork") dependsOn manpage ~ SBuildConfig.scalaLibrary ~ asciidoctor ~ "scan:.sbuild/scala/SBuild.scala" ~ jruby exec { ctx: TargetContext =>
+    addons.support.ForkSupport.runJavaAndWait(
+      classpath = (SBuildConfig.scalaLibrary ~ asciidoctor ~ jruby).files ++ Seq(Path(".sbuild/scala/SBuild.scala")),
+      arguments = Array(
+        "AsciiDoctorRunner",
+        "backend=html5",
+        "targetDir=" + Path("target").getPath,
+        manpage.files.head.getPath)
+    )
+  }
+
+  Target("phony:manual") dependsOn "manual-native"
+
+  Target("phony:manpage-native").cacheable dependsOn manpage ~ "scan:/src/manpage" exec {
+    val targetDir = Path("target/manpage")
+    targetDir.mkdirs
+    val result = addons.support.ForkSupport.runAndWait(
+      Array("a2x", "-v", "-D", targetDir.getPath, "--format", "manpage", Path(manpage).getPath, "-a", "revversion=" + SBuildConfig.sbuildVersion)
+    )
+
+    if(result != 0) throw new RuntimeException("a2x failed. Return code: " + result)
+  }
+
+  Target("phony:manual-native").cacheable dependsOn manual ~ "scan:src/manual" exec {
+    val targetDir = Path("target/manual")
+    targetDir.mkdirs
+    val result = addons.support.ForkSupport.runAndWait(
+      Array("a2x", "-v", "-D", targetDir.getPath, "--format", "xhtml", Path(manual).getPath, "--icons", "-a", "revversion=" + SBuildConfig.sbuildVersion)
+    )
+
+    if(result != 0) throw new RuntimeException("a2x failed. Return code: " + result)
+  }
+
+  Target("phony:manual-asciidoctor") dependsOn manual ~ SBuildConfig.scalaLibrary ~ asciidoctor ~ "scan:.sbuild/scala/SBuild.scala" ~ jruby exec { ctx: TargetContext =>
+    import java.net.URL
+
+    @scala.annotation.tailrec
+    def printCp(cl: ClassLoader) {
+      val cp = cl match {
+        case null => return
+        case cl: { def getURLs: Array[URL] } => "\n  " + cl.getURLs.map(_.toExternalForm).mkString("\n  ")
+        case _ => "Not able to extract classloader URLs."
+      }
+      println("Classpath of ClassLoader: " + cl + ": " + cp)
+      printCp(cl.getParent)
+    }
+
+    printCp(getClass.getClassLoader)
+
+    new AsciiDoctor(
+      backend = Some("html5"),
+      targetDir = Some("target"),
+      sourceFiles = manual.files
+    ).execute
+  }
+
+  Target("phony:manual-asciidoctor-fork") dependsOn manual ~ SBuildConfig.scalaLibrary ~ asciidoctor ~ "scan:.sbuild/scala/SBuild.scala" ~ jruby exec { ctx: TargetContext =>
+
+    val result = addons.support.ForkSupport.runJavaAndWait(
+      classpath = Seq(Path(".sbuild/scala/SBuild.scala")) ++ (SBuildConfig.scalaLibrary ~ asciidoctor ~ jruby).files,
+      arguments = Array(
+        "AsciiDoctorRunner",
+        "backend=html5",
+        //        "baseDir=" + Path("src/manual").getPath,
+        "targetDir=" + Path("target").getPath
+      ) ++
+        manual.files.map(_.getPath)
+    )
+
+    if(result != 0) throw new RuntimeException("AsciiDoctorRunner failed. Return code: " + result)
+    ???
+  }
+
+}
+
+import java.io.File
+import scala.collection.JavaConverters._
+import org.asciidoctor.Asciidoctor
+import org.asciidoctor.Options
+import org.asciidoctor.Attributes
+
+object AsciiDoctorRunner {
+  def main(args: Array[String]): Unit = {
+
+    case class Args(
+      backend: Option[String] = None,
+      targetDir: Option[String] = None,
+      sourceFiles: Seq[File] = Seq(),
+      baseDir: Option[String] = None,
+      gemPath: Option[String] = None)
+
+    val parsed = args.foldLeft(Args()) { (args, param) =>
+      param.split("=", 2) match {
+        case Array("backend", backend) => args.copy(backend = Some(backend))
+        case Array("targetDir", targetDir) => args.copy(targetDir = Some(targetDir))
+        case Array("gemPath", gemPath) => args.copy(gemPath = Some(gemPath))
+        case Array("baseDir", baseDir) => args.copy(baseDir = Some(baseDir))
+        case Array(sourceFile) => args.copy(sourceFiles = new File(sourceFile) +: args.sourceFiles)
+        case Array(unsupported, _) =>
+          throw new RuntimeException("Unsupported parameter: " + unsupported +
+            "\nUsage: main [backend=backend] [targetDir=targetDir] sourceFile+")
+      }
+    }
+
+    new AsciiDoctor(
+      backend = parsed.backend,
+      targetDir = parsed.targetDir,
+      sourceFiles = parsed.sourceFiles,
+      gemPath = parsed.gemPath,
+      baseDir = parsed.baseDir
+    ).execute
+  }
+}
+
+class AsciiDoctor(backend: Option[String],
+                  targetDir: Option[String],
+                  sourceFiles: Seq[File],
+                  baseDir: Option[String] = None,
+                  docType: Option[String] = None,
+                  gemPath: Option[String] = None) {
+
+  def execute: Unit = {
+
+    val attributes = new Attributes()
+    val options = new Options()
+
+    options.setMkDirs(true)
+
+    backend.map(options.setBackend(_))
+    docType.map(options.setDocType(_))
+    baseDir.map(d => options.setBaseDir(d))
+    targetDir match {
+      case Some(d) => options.setToDir(d)
+      case None => options.setInPlace(true)
+    }
+
+    options.setAttributes(attributes)
+
+    val asciidoctor = Asciidoctor.Factory.create()
+    asciidoctor.renderFiles(sourceFiles.toList.asJava, options)
+
   }
 
 }
