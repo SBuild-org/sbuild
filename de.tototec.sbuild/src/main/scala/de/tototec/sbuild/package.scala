19bb628cdcc9392708bf746577a282181d806dda
Tobias Roeser <le.petit.fou@web.de>
2013-11-01 16:20:05 +0000

    Also provide TargetRefs implicit conversions from sbuild package object.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/package.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/package.scala
@@ -2,25 +2,26 @@ package de.tototec
 
 /**
  * ==SBuild API Reference==
- * 
+ *
  * This package contains SBuild core classes and public API.
- * 
+ *
  * Project Homepage: [[http://sbuild.tototec.de/]]
  *
  * SBuild is released under the [[http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0]].
  *
- * 
+ *
  * ===SBuild API to be used in SBuild build scripts===
- * 
+ *
  * The following classes form SBUild's public API:
- * 
+ *
  *  - `[[classpath @classpath]]`
  *  - `[[ExportDependencies]]`
  *  - `[[include @include]]`
+ *  - `[[Logger]]`
  *  - `[[Module]]`
  *  - `[[Modules]]`
  *  - `[[Path]]`
- *  - `[[Prop]]` 
+ *  - `[[Prop]]`
  *  - `[[SchemeHandler]]`
  *  - `[[SchemeResolver]]`
  *  - `[[SchemeResolverWithDependencies]]`
@@ -30,21 +31,27 @@ package de.tototec
  *  - `[[TargetRef]]`
  *  - `[[TargetRefs]]`
  *  - `[[version @version]]`
- * 
- * Classes and objects, not listed above are candidates to change in succeeding releases. 
+ *
+ *  The following classes are currently experimental or not part of the public API:
+ *  - `[[Plugin]]`
+ *  - `[[ResolveFiles]]`
+ *  - `[[ResolveResult]]`
+ *  - `[[ServiceRegistry]]`
+ *
+ * Classes and objects, not listed above are candidates to change in succeeding releases.
  * You are encouraged, to use only the API form above in your build scripts.
- * 
+ *
  * ===Default Scheme Handlers===
- * 
+ *
  * The following list of `[[SchemeHandler]]` implementations are registered by-default with their default configuration in each SBuild project.
- * To register or overwrite a scheme handler in your project, you should use `[[SchemeHandler$.apply]]`.   
- * 
+ * To register or overwrite a scheme handler in your project, you should use `[[SchemeHandler$.apply]]`.
+ *
  *  - `[[HttpSchemeHandler]]`
  *  - `[[MvnSchemeHandler]]`
  *  - `[[ZipSchemeHandler]]`
  *  - `[[ScanSchemeHandler]]`
- * 
+ *
  */
-package object sbuild {
-
+package object sbuild extends TargetRefsImplicits {
+  
 }
