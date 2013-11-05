6914f6ff08dd7d4c74a8226e7ed7c0953ca3024a
Tobias Roeser <le.petit.fou@web.de>
2013-11-05 22:57:28 +0000

    Added target factory for xgettext.

--- SBuildConfig.scala
+++ SBuildConfig.scala
@@ -1,5 +1,6 @@
 import de.tototec.sbuild._
 import de.tototec.sbuild.TargetRefs._
+import de.tototec.sbuild.ant.tasks._
 
 object SBuildConfig {
 
@@ -28,5 +29,36 @@ object SBuildConfig {
   val log4jOverSlf4j = "mvn:org.slf4j:log4j-over-slf4j:1.7.5"
   val logbackCore = "mvn:ch.qos.logback:logback-core:1.0.13"
   val logbackClassic = "mvn:ch.qos.logback:logback-classic:1.0.13"
+
+}
+
+class I18n()(implicit _project: Project) {
+
+  def applyAll {
+
+  val msgSources = "src/main/scala"
+  val msgCatalog = "target/po/messages.pot"
+
+  Target("phony:xgettext") dependsOn msgCatalog
+
+  Target(msgCatalog) dependsOn s"scan:${msgSources}" exec { ctx: TargetContext =>
+    ctx.targetFile.get.getParentFile.mkdirs
+
+    import java.io.File
+    val srcDirUri = Path(msgSources).toURI
+
+    AntExec(
+      failOnError = true,
+      executable = "xgettext",
+      args = Array[String](
+        "-ktr", "-kmarktr",
+        "--language", "Java",
+        "--directory", new File(srcDirUri).getPath,
+        "--output-dir", ctx.targetFile.get.getParent,
+        "--output", ctx.targetFile.get.getName) ++
+        s"scan:${msgSources}".files.map(file => srcDirUri.relativize(file.toURI).getPath)
+    )
+  }
+  }
 
 }
