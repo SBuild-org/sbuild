f65e1b4b99927fe29dc054a418cf50e54fdc5143
Tobias Roeser <le.petit.fou@web.de>
2013-11-09 12:24:17 +0000

    Added Constants object.

---
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/Constants.scala
@@ -0,0 +1,17 @@
+package de.tototec.sbuild
+
+object Constants {
+
+  /**
+   * The package(s) part of the plugins public API.
+   * The packages will be part of the current projects classpath.
+   * All other packages of that plugin will only be available to the plugin implementation itself.
+   */
+  val SBuildPluginExportPackage = "SBuild-PluginExportPackage"
+
+  /**
+   * The full qualified class name of the plugin implementation class, which must implement [[Plugin]].
+   */
+  val SBuildPluginFactory = "SBuild-PluginFactory"
+
+}
