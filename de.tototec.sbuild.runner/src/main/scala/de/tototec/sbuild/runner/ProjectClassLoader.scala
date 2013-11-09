64e0dcdbce90c707dc37d9527918ce3e54efd2c7
Tobias Roeser <le.petit.fou@web.de>
2013-11-09 12:23:48 +0000

    Added ProjectClassLoader and PluginClassLoader.

---
+++ de.tototec.sbuild.runner/src/main/scala/de/tototec/sbuild/runner/ProjectClassLoader.scala
@@ -0,0 +1,49 @@
+package de.tototec.sbuild.runner
+
+import java.net.URLClassLoader
+import java.net.URL
+import scala.util.Try
+import scala.util.Success
+import scala.util.Failure
+import java.util.jar.JarInputStream
+import de.tototec.sbuild.Constants
+import de.tototec.sbuild.Logger
+
+/**
+ * This classloader first tried to load all classes from the given parent classloader or the classpathUrls.
+ * If that fails, it tries to load the classes from internally maintained plugin classloader,
+ * but it will only load those classes which are exported by that plugin. [[de.tototec.sbuild.Constants.SBuildPluginExportPackage]]
+ *
+ */
+class ProjectClassLoader(classpathUrls: Array[URL], pluginUrls: Array[URL], parent: ClassLoader)
+    extends URLClassLoader(classpathUrls, parent) {
+
+  ClassLoader.registerAsParallelCapable()
+
+  val pluginClassLoaders: Seq[PluginClassLoader] = pluginUrls.toSeq.map(url => new PluginClassLoader(url, this))
+
+  override protected def loadClass(className: String, resolve: Boolean): Class[_] = getClassLoadingLock(className).synchronized {
+    try {
+      super.loadClass(className, resolve)
+    } catch {
+      case e: ClassNotFoundException =>
+        LookupCycleGuard.get match {
+          case null => try {
+            LookupCycleGuard.set(className)
+            pluginClassLoaders.toIterator.map { pluginClassLoader =>
+              Try[Class[_]](pluginClassLoader.loadPluginClass(className))
+            }.find(_.isSuccess) match {
+              case Some(Success(loadedClass)) => loadedClass
+              case _ => throw new ClassNotFoundException(className)
+            }
+          } finally {
+            LookupCycleGuard.set(null)
+          }
+          case cl => throw e
+        }
+    }
+  }
+
+}
+
+private object LookupCycleGuard extends ThreadLocal[String] 
---
+++ de.tototec.sbuild.runner/src/test/scala/de/tototec/sbuild/runner/PluginClassLoaderTest.scala
@@ -0,0 +1,24 @@
+package de.tototec.sbuild.runner
+
+import org.scalatest.FunSuite
+
+class PluginClassLoaderTest extends FunSuite {
+
+  val underTest = new PluginClassLoader(null, null) {
+    override lazy val allowedPackageNames: Seq[String] = Seq("a.b.c", "a.b.c.d")
+  }
+
+  val cases = Seq(
+    "a.b.c.Class" -> true,
+    "a.b.c.Class.SubClass" -> true,
+    "a.b.c.d.Class" -> true,
+    "a.b.Class" -> false
+  )
+
+  cases.map {
+    case (p, a) => test("Package " + p + " should be " + (if(a) "" else "not ") + "allowed") {
+     assert( underTest.checkClassNameInExported(p) === a)
+    }
+  }
+
+}
