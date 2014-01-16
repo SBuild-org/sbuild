5566acfe0b01ca95c28826203db172e6bfd7b7e0
Tobias Roeser <le.petit.fou@web.de>
2014-01-16 14:22:35 +0100

    Added StringSplitter.

---
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/internal/StringSplitter.scala
@@ -0,0 +1,37 @@
+package de.tototec.sbuild.internal
+
+import java.util.ArrayList
+
+object StringSplitter {
+
+  def split(stringToSplit: String,
+            delim: String,
+            delimMask: Option[String] = None,
+            maxCount: Int = 0): Seq[String] = {
+
+    val delimMaskLength = delimMask match {
+      case Some(m) => m.length
+      case None => 0
+    }
+
+    /** Find next part to be split and return the rest. */
+    def find(parts: Seq[String], current: String, rest: String): Seq[String] = {
+      if (rest == "") {
+        parts ++ Seq(current)
+      } else if (rest.startsWith(delim)) {
+        find(parts ++ Seq(current), "", rest.substring(delimMaskLength))
+      } else if (delimMask.isDefined && rest.startsWith(delimMask.get)) {
+        if (rest.size > delimMaskLength) {
+          find(parts, current + rest.substring(delimMaskLength, delimMaskLength + 1), rest.substring(delimMaskLength + 1))
+        } else {
+          parts ++ Seq(current + rest)
+        }
+      } else {
+        find(parts, current + rest.substring(0, 1), rest.substring(1))
+      }
+    }
+
+    find(Seq(), "", stringToSplit)
+  }
+
+}
