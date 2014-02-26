4163b58a4c5b2604732b2eb9ba0b74f0f855448c
Tobias Roeser <le.petit.fou@web.de>
2014-02-26 16:53:24 +0100

    Reordered val's to avoid invalid initialization order.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/Constants.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/Constants.scala
@@ -7,45 +7,45 @@ object Constants {
    * The packages will be part of the current projects classpath.
    * All other packages of that plugin will only be available to the plugin implementation itself.
    */
-  @deprecated("Use ManifestSBuildExportPackage instead.", "0.7.1.9000")
-  val SBuildPluginExportPackage = ManifestSBuildExportPackage
+  val ManifestSBuildExportPackage = "SBuild-ExportPackage"
   /**
    * The package(s) part of the plugins public API.
    * The packages will be part of the current projects classpath.
    * All other packages of that plugin will only be available to the plugin implementation itself.
    */
-  val ManifestSBuildExportPackage = "SBuild-ExportPackage"
+  @deprecated("Use ManifestSBuildExportPackage instead.", "0.7.1.9000")
+  val SBuildPluginExportPackage = ManifestSBuildExportPackage
 
+  /**
+   * A pair of full qualified class names denoting the plugin instance class and it factory delimited by an equals (=) sign. The factory class must implement [[Plugin]].
+   */
+  val ManifestSBuildPlugin = "SBuild-Plugin"
   /**
    * A pair of full qualified class names denoting the plugin instance class and it factory delimited by an equals (=) sign. The factory class must implement [[Plugin]].
    */
   @deprecated("Use ManifestSBuildPlugin instead.", "0.7.1.9000")
   val SBuildPlugin = ManifestSBuildPlugin
-  /**
-   * A pair of full qualified class names denoting the plugin instance class and it factory delimited by an equals (=) sign. The factory class must implement [[Plugin]].
-   */
-  val ManifestSBuildPlugin = "SBuild-Plugin"
 
   val ManifestSBuildExportClasspath = "SBuild-ExportClasspath"
 
   /**
    * Addional classpath which will be automatically resolved and used when this header is present in a plugin JAR.
    */
-  @deprecated("Use ManifestSBuildClasspath instead.", "0.7.1.9000")
-  val SBuildPluginClasspath = ManifestSBuildClasspath
+  val ManifestSBuildClasspath = "SBuild-Classpath"
   /**
    * Addional classpath which will be automatically resolved and used when this header is present in a plugin JAR.
    */
-  val ManifestSBuildClasspath = "SBuild-Classpath"
+  @deprecated("Use ManifestSBuildClasspath instead.", "0.7.1.9000")
+  val SBuildPluginClasspath = ManifestSBuildClasspath
 
+  /**
+   * The minimal SBuild version required to run this plugin.
+   */
+  val ManifestSBuildVersion = "SBuild-Version"
   /**
    * The minimal SBuild version required to run this plugin.
    */
   @deprecated("Use ManifestSBuildVersion instead.", "0.7.1.9000")
   val SBuildVersion = ManifestSBuildVersion
-  /**
-   * The minimal SBuild version required to run this plugin.
-   */
-  val ManifestSBuildVersion = "SBuild-Version"
 
 }
