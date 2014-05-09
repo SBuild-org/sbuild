e80d9b419b481bbc844a03e654303255e02f2303
Tobias Roeser <le.petit.fou@web.de>
2014-05-09 13:48:09 +0200

    Added a experimental list-files scheme handler.

--- org.sbuild.runner.bootstrap/src/main/scala/org/sbuild/runner/bootstrap/SBuildBootstrap.scala
+++ org.sbuild.runner.bootstrap/src/main/scala/org/sbuild/runner/bootstrap/SBuildBootstrap.scala
@@ -3,11 +3,14 @@ package org.sbuild.runner.bootstrap
 import org.sbuild.Project
 import org.sbuild.ScanSchemeHandler
 import org.sbuild.SchemeHandler
+import org.sbuild.SchemeResolverWithDependencies
 import org.sbuild.plugins.http.HttpSchemeHandler
 import org.sbuild.plugins.mvn.MvnSchemeHandler
 import org.sbuild.plugins.unzip.UnzipSchemeHandler
 import org.sbuild.Plugin
 import org.sbuild.MapperSchemeHandler
+import org.sbuild.TargetRefs
+import org.sbuild.TargetContext
 
 class SBuildBootstrap(implicit _project: Project) {
 
@@ -30,6 +33,15 @@ class SBuildBootstrap(implicit _project: Project) {
       case path if path.startsWith("mvn:") => path + ";classifier=sources"
     }: PartialFunction[String, String])
   }
+
+  // Experimental!
+  SchemeHandler("show-files", new SchemeHandler with SchemeResolverWithDependencies {
+    def localPath(schemeContext: SchemeHandler.SchemeContext): String = s"phony:${schemeContext.fullName}"
+    def dependsOn(schemeContext: SchemeHandler.SchemeContext): TargetRefs = schemeContext.path
+    def resolve(schemeContext: SchemeHandler.SchemeContext, targetContext: TargetContext): Unit = {
+      println(s"Files of target ${schemeContext.path}:\n${targetContext.fileDependencies.mkString("\n")}")
+    }
+  })
 
   // Experimental: replace by plugin
   //  SchemeHandler("source", new MapperSchemeHandler(
