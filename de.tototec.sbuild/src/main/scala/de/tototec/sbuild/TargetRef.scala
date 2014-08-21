e95b2a576b904b59517a1adc482281312ce6a471
Tobias Roeser <le.petit.fou@web.de>
2014-08-21 22:07:50 +0200

    Replaced getName by getPath in targetProject implementation.

--- de.tototec.sbuild/src/main/scala/de/tototec/sbuild/TargetRef.scala
+++ de.tototec.sbuild/src/main/scala/de/tototec/sbuild/TargetRef.scala
@@ -66,7 +66,7 @@ class TargetRef(val ref: String)(implicit project: Project) {
     if (explicitProject == None || project.projectFile == explicitProject.get)
       Some(project)
     else
-      project.findModule(explicitProject.get.getName)
+      project.findModule(explicitProject.get.getPath)
 
   protected[sbuild] def safeTargetProject: Project =
     targetProject match {
